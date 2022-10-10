a26bdd4041d0c7e67b681bf78f1ffbda169861ca
Doug Johnson <doug@stormtree.ca>
2022-10-10 14:43:37 -0700

    Refactor out TextPane and ListPane

--- src/components/ContentPane.js
+++ src/components/ContentPane.js
@@ -7,7 +7,15 @@ export default function ContentPane() {
       p={4}
       gap='10px'
     >
-      <Box flex='1'>
+      <TextPane flex='1' />
+      <ListPane flex='1' />
+    </Flex>
+  )
+}
+
+function TextPane(props) {
+  return (
+    <Box {...props}>
         <Heading>
           Story
         </Heading>
@@ -15,7 +23,12 @@ export default function ContentPane() {
           The Sensitive Ecosystems Inventory (SEI) of East Vancouver Island project has been responsible for tracking sensitive ecosystems for many regions within the Capital Regional District (CRD). Their data is what you see on the map. Their research identifies different sensitive ecosystems and allows us to understand better how these ecosystems are at risk. Some of the most significant factors responsible for the degradation and ecosystem damage are human usage and the introduction of invasive species. These ecosystems are shrinking, often due to development or land use changes. The areas on this map do not include private lands, provincial & federal lands, or Indigenous reservations. It is important to understand that each of these ecosystems plays a crucial role in the ecological well-being of this area. Damage to one sensitive ecosystem will affect another. These ecosystems in this area of the coast are known for their biodiversity. Researchers in Japan found that after “forest bathing,” participants had been breathing in phytoncides (essential wood oils) that had antimicrobial properties. Participants had increased NK (natural killer) cells, responsible for the immune response that kills tumours and viruses. Another study sprayed cedar essential oils in a hotel room before guests stayed, which had the same effect on their immune systems. So if you need an excuse to ditch school and go to the beach or buy more house plants, do it! It’s good for your health!
         </Text>
       </Box>
-      <Box flex='1'>
+  )
+}
+
+function ListPane(props) {
+  return (
+    <Box {...props}>
         <Heading>
           Tips
         </Heading>
@@ -30,6 +43,5 @@ export default function ContentPane() {
           <ListItem>Be a mindful water user - use rainwater systems to water your garden, plants, and gardens during cooler hours to prevent evaporation.</ListItem>
         </OrderedList>
       </Box>
-    </Flex>
   )
 }
