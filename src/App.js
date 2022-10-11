45363e2efc864a7d2cc8bd79d6f020ec206de7d8
Doug Johnson <43759986+doug-stormtree@users.noreply.github.com>
2022-10-10 22:52:07 -0700

    Data layers in MapView (#7)
    * Refactor out TextPane and ListPane
    
    * Fix layout and add collapsing drawer
    
    * Add and display Heat Map data
    
    * Fix duplicate props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import MapView from './components/MapView';
 import ContentPane from './components/ContentPane';
 import { Flex } from '@chakra-ui/react';
 import React, { useRef } from 'react';
+import * as HeatDomes from './data/HeatDomes.json';
+import * as AC_Buildings from './data/AC_Buildings.json';
+import { GeoJSON } from 'react-leaflet/GeoJSON';
+import { divIcon, marker } from 'leaflet';
+import ReactDOMServer from "react-dom/server";
+import { ReactComponent as CommunityCtrIcon } from './svg/cc.svg';
+import { ReactComponent as LibraryIcon } from './svg/lib.svg';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -22,9 +29,39 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-console.log(app.name);
+if (app === null) {console.log("Firebase did not initialize.")};
 
 function App() {
+  const style_HeatDomes = {
+    opacity: 0.5,
+    color: 'rgba(189,17,20,1.0)',
+    dashArray: '',
+    lineCap: 'butt',
+    lineJoin: 'miter',
+    weight: 1.0,
+    fill: true,
+    fillOpacity: 0.7,
+    fillColor: 'rgba(189,17,20,1.0)',
+    interactive: true,
+  }
+
+  function point(feature, latlng){
+    const icon = feature.properties.Type === "Library" ? (
+        <LibraryIcon />
+      ) : (
+        <CommunityCtrIcon />
+      );
+
+    return marker(latlng, {
+      icon: divIcon({
+        className: "",
+        iconSize: [40, 40],
+        iconAnchor: [24, 24],
+        html: ReactDOMServer.renderToString(icon),
+      })
+    });
+  }
+
   const map = useRef(null);
 
   const invalidateMap = () => {
@@ -40,7 +77,16 @@ function App() {
       overflow='hidden'
     >
       <NavBar flex='0'/>
-      <MapView flex='1' h='100%' mapRef={map} />
+      <MapView flex='1' h='100%' mapRef={map}>
+        <GeoJSON
+          data={AC_Buildings}
+          pointToLayer={(a, b) => point(a, b)}
+        />
+        <GeoJSON
+          data={HeatDomes}
+          style={style_HeatDomes}
+        />
+      </MapView>
       <ContentPane flex='0' h='md' onToggle={invalidateMap}/>
     </Flex>
   );
