fd08ad504d23d28265d967e32039b21a9a37f152
Doug Johnson <doug@stormtree.ca>
2022-11-07 22:28:15 -0800

    Define responsive Modal sizes, move to jsx

--- src/components/LandingModalButton.jsx
+++ src/components/LandingModalButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { 
+import {
+  useBreakpoint,
   useDisclosure,
   Button,
   Modal,
@@ -9,28 +10,40 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Stack,
+  Text,
 } from '@chakra-ui/react';
+import {
+  TerritoryAcknowledgement,
+  RushMissionStatement
+} from '../data/TextContent';
 
 export default function LandingModalButton(props) {
   const { isOpen, onOpen, onClose } = useDisclosure(true);
+  const bp = useBreakpoint();
   return (
     <>
       <Button onClick={onOpen} {...props}>{props.children}</Button>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size={{
+        xl: "4xl",
+        lg: "xl",
+        md: "xl",
+        sm: "full",
+        base: "full",
+      }}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Welcome to the RUSH Initiative Web Map</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            Hello body text.
+            <Stack spacing='1em'>
+              <Text>{ RushMissionStatement }</Text>
+              <Text>{ TerritoryAcknowledgement }</Text>
+            </Stack>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
