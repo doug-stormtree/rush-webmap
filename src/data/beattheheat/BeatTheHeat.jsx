082d862c97f319c8084b7e330c613185e21bbf67
Doug Johnson <doug@stormtree.ca>
2022-11-15 21:43:54 -0800

    Update BeatTheHeat text

--- src/data/beattheheat/BeatTheHeat.jsx
+++ src/data/beattheheat/BeatTheHeat.jsx
@@ -11,9 +11,8 @@ const BeatTheHeat = {
   question: "How can I keep myself and the ecosystem cool & hydrated?",
   learn: {
     text: [
-      "The Sensitive Ecosystems Inventory (SEI) of East Vancouver Island project has been responsible for tracking sensitive ecosystems for many regions within the Capital Regional District (CRD). Their data is what you see on the map. Their research identifies different sensitive ecosystems and allows us to understand better how these ecosystems are at risk.",
-      "Some of the most significant factors responsible for the degradation and ecosystem damage are human usage and the introduction of invasive species. These ecosystems are shrinking, often due to development or land use changes. The areas on this map do not include private lands, provincial & federal lands, or Indigenous reservations. It is important to understand that each of these ecosystems plays a crucial role in the ecological well-being of this area. Damage to one sensitive ecosystem will affect another. These ecosystems in this area of the coast are known for their biodiversity.",
-      "Researchers in Japan found that after “forest bathing,” participants had been breathing in phytoncides (essential wood oils) that had antimicrobial properties. Participants had increased NK (natural killer) cells, responsible for the immune response that kills tumours and viruses. Another study sprayed cedar essential oils in a hotel room before guests stayed, which had the same effect on their immune systems. So if you need an excuse to ditch school and go to the beach or buy more house plants, do it! It's good for your health!"
+      "All life is vulnerable to extreme heat. Heat islands and heat waves happen as average global temperatures rise, and hotter pockets of air move inland. The red polygons show areas with significant increases in average summer surface temperature from 2018 to 2021 within the CRD. Notice the relationship between heat island areas, green space and dark or paved surfaces. Urban centres with dark pavement can absorb the heat and re-emit it at a rate of 95%. Breezes and closeness to water also affect temperature.",
+      "Prolonged heat dries out landscapes, increases risk of fires, can shorten growing seasons for farmers and create conditions for viruses and bacteria to thrive. While plants cool the area and drawdown harmful greenhouse gases from the atmosphere, the soil processes the gases at a rate of 2:1 compared to plants. The partnership between soil and the atmosphere is key to having a healthy climate. Taking action to mange temperature and moisture in our neighbourhoods is a key to climate action.",
     ],
     initiatives: [
       { 
@@ -25,14 +24,10 @@ const BeatTheHeat = {
   },
   act: {
     list: [
-      "Learn to identify invasive species and how to remove them safely. Get involved with local efforts to remove invasive species in your area. Check out the events map to learn more!",
-      "Learn about what native species are in your area. Look for local native plant sales when you shop for indoor and outdoor plants.",
-      "Avoid the use of pesticides and chemicals in your garden.",
-      "Next time you visit the beach or go on a hike, leave the speaker at home! Take a moment to enjoy the sounds of nature. Loud noises can disturb birds and cause them to abandon their eggs. Loud noise can also disrupt communication between wildlife (e.g., birds).",
-      "Enjoy wildlife from afar. Avoid approaching or touching wildlife or their nesting habitats.",
-      "Stay on designated paths and trails to avoid damaging vegetation and wildlife habitats.",
-      "Keep your pet on a leash when taking them on walks to avoid damage to sensitive ecosystems.",
-      "Be a mindful water user - use rainwater systems to water your garden, plants, and gardens during cooler hours to prevent evaporation."
+      "Drink plenty of water and stay cool! Seek out green spaces, shade, water bodies or air-conditioned buildings.",
+      "Grow your garden! Create shade in your own living spaces.",
+      "Ask your municipality for more green space and less pavement.",
+      "Limit your carbon footprint! Biking, carpooling and taking the bus are some of the easiest ways to reduce your carbon footprint and live sustainably.",
     ],
     initiatives: [
       { 
