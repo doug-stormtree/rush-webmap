2e07b035eba42e20c0e28d406292cabefaad76fc
Doug Johnson <doug@stormtree.ca>
2022-12-03 18:14:06 -0800

    Add Pollinator Data with initial display

--- src/data/naturehood/Naturehood.jsx
+++ src/data/naturehood/Naturehood.jsx
@@ -1,4 +1,31 @@
 import image from './image.jpg';
+import * as Pollinators from './Pollinators.json';
+import { ReactComponent as IconA } from './5c311b3537407e1f05ac3946.svg';
+import { ReactComponent as IconB } from './5c312b3c37407e1f05ac3950.svg';
+import { ReactComponent as IconC } from './5c312ca837407e1f05ac3952.svg';
+import { ReactComponent as IconD } from './5ca7bfc0ecd8490100cab980.svg';
+import { ReactComponent as IconE } from './5ca7bfe1ecd8490100cab9d5.svg';
+import { ReactComponent as IconF } from './5ca7bfe8ecd8490100cab9ed.svg';
+import { ReactComponent as IconG } from './5ca7bfe8ecd8490100cab9ef.svg';
+import { ReactComponent as IconH } from './5ca7bfe9ecd8490100cab9f0.svg';
+import { ReactComponent as IconI } from './5ca7bfe9ecd8490100cab9f1.svg';
+import { ReactComponent as IconJ } from './5ca7bfe9ecd8490100cab9f2.svg';
+import { ReactComponent as IconK } from './5ca7bfeaecd8490100cab9f3.svg';
+import { ReactComponent as IconL } from './5ca7bfeaecd8490100cab9f5.svg';
+import { ReactComponent as IconM } from './5ca7bfececd8490100cab9fa.svg';
+import { ReactComponent as IconN } from './5ca7bff0ecd8490100caba08.svg';
+import { ReactComponent as IconO } from './5ca7bff2ecd8490100caba0c.svg';
+import { ReactComponent as IconP } from './5ca7bff2ecd8490100caba0d.svg';
+import { ReactComponent as IconQ } from './5ca7bff4ecd8490100caba0f.svg';
+import { ReactComponent as IconR } from './5ca7bff4ecd8490100caba10.svg';
+import { ReactComponent as IconS } from './5ca7bff5ecd8490100caba11.svg';
+import { ReactComponent as IconT } from './5ca7bff6ecd8490100caba12.svg';
+import { ReactComponent as IconU } from './5ca7bff8ecd8490100caba13.svg';
+import { ReactComponent as IconV } from './5ca7bff9ecd8490100caba14.svg';
+import { ReactComponent as IconW } from './5ca7bff9ecd8490100caba15.svg';
+import { ReactComponent as IconX } from './5ca7bff9ecd8490100caba16.svg';
+import { ReactComponent as IconY } from './5ca7bff9ecd8490100caba17.svg';
+import { ReactComponent as IconZ } from './5ca7bffaecd8490100caba18.svg';
 
 const Naturehood = {
   title: "Neighbourhood to Naturehood?",
@@ -17,6 +44,41 @@ const Naturehood = {
     initiatives: [],
   },
   mapData: [
+    {
+      title: "Pollinators",
+      patch: '<div style="width: 1em; height: 1em; background: #ae163e;"></div>',
+      data: Pollinators,
+      format: 'point',
+      property: "icon",
+      propertyMap: {
+        '5c311b3537407e1f05ac3946': (<IconA style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5c312b3c37407e1f05ac3950': (<IconB style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5c312ca837407e1f05ac3952': (<IconC style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfc0ecd8490100cab980': (<IconD style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfe1ecd8490100cab9d5': (<IconE style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfe8ecd8490100cab9ed': (<IconF style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfe8ecd8490100cab9ef': (<IconG style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfe9ecd8490100cab9f0': (<IconH style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfe9ecd8490100cab9f1': (<IconI style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfe9ecd8490100cab9f2': (<IconJ style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfeaecd8490100cab9f3': (<IconK style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfeaecd8490100cab9f5': (<IconL style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bfececd8490100cab9fa': (<IconM style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff0ecd8490100caba08': (<IconN style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff2ecd8490100caba0c': (<IconO style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff2ecd8490100caba0d': (<IconP style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff4ecd8490100caba0f': (<IconQ style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff4ecd8490100caba10': (<IconR style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff5ecd8490100caba11': (<IconS style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff6ecd8490100caba12': (<IconT style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff8ecd8490100caba13': (<IconU style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff9ecd8490100caba14': (<IconV style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff9ecd8490100caba15': (<IconW style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff9ecd8490100caba16': (<IconX style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bff9ecd8490100caba17': (<IconY style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+        '5ca7bffaecd8490100caba18': (<IconZ style={{borderRadius: '100%', backgroundColor: 'rgba(255,255,255,0.8)', }} />),
+      },
+    },
   ],
 };
 export default Naturehood;
