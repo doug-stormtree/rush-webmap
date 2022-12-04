ad4930722c224a0da79d01c4216d697e6a824c4f
Doug Johnson <doug@stormtree.ca>
2022-12-04 15:08:45 -0800

    Add Question Placeholders

---
+++ src/data/circularecon/CircularEcon.jsx
@@ -0,0 +1,22 @@
+import image from './image.jpg';
+
+const CircularEcon = {
+  title: 'Circular Economy?',
+  question: 'How can I participate in circular economies?',
+  image: image,
+  learn: {
+    text: [
+      "Lorem.",
+    ],
+    initiatives: [],
+  },
+  act: {
+    list: [
+      "Lorem.",
+    ],
+    initiatives: [],
+  },
+  mapData: [
+  ],
+};
+export default CircularEcon;
---
+++ src/data/flooding/Flooding.jsx
@@ -0,0 +1,22 @@
+import image from './image.jpg';
+
+const Flooding = {
+  title: 'Protect from Flooding?',
+  question: 'How is my community at risk from flooding?',
+  image: image,
+  learn: {
+    text: [
+      "Lorem.",
+    ],
+    initiatives: [],
+  },
+  act: {
+    list: [
+      "Lorem.",
+    ],
+    initiatives: [],
+  },
+  mapData: [
+  ],
+};
+export default Flooding;
