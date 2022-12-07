965ebfde73e699af0516031d0e83ccc7565b95bc
Doug Johnson <doug@stormtree.ca>
2022-12-06 22:36:40 -0800

    Generate share URLs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,14 +6,17 @@ import {
   useStyleConfig,
 } from '@chakra-ui/react';
 import LandingModalButton from './LandingModalButton';
+import ShareModalButton from './ShareModalButton';
 
 export default function NavBar(props) {
   const styles = useStyleConfig('NavBar');
+  const { getShareURL, ...rest } = props;
 
   return (
-    <Box __css={styles} {...props} >
+    <Box __css={styles} {...rest} >
       <Text size="xl" m="auto 1em">RUSH</Text>
       <Spacer />
+      <ShareModalButton getShareURL={getShareURL} />
       <LandingModalButton />
     </Box>
   )
---
+++ src/components/ShareModalButton.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {
+  useClipboard,
+  useDisclosure,
+  Button,
+  Flex,
+  Icon,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from '@chakra-ui/react';
+import { FiShare } from 'react-icons/fi';
+import { FaRegCopy } from 'react-icons/fa';
+
+export default function ShareModalButton(props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [url, setUrl] = useState("https://whatstherush.ca/")
+  const { onCopy, value, hasCopied } = useClipboard(url);
+
+  return (
+    <>
+      <IconButton
+        variant='nav'
+        icon={<FiShare />}
+        onClick={() => {
+          onOpen();
+          setUrl(props.getShareURL());
+        }}
+      />
+      <Modal
+        isCentered
+        isOpen={isOpen}
+        onClose={onClose}
+        size={{
+          xl: "4xl",
+          md: "xl",
+          base: "full",
+        }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Share this map</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Use this link to share the current question and map view</Text>
+            <Flex my={2}>
+              <Input
+                readOnly
+                value={value}
+                mr={2}
+              />
+              <Button onClick={onCopy}>
+                {hasCopied ? "Copied!" : "Copy"}
+                <Icon as={FaRegCopy} marginInlineStart='0.4em' />
+              </Button>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
