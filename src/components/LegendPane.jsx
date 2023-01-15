fd04e0e948c0271f0029235a91dbf94c890d55cf
Doug Johnson <doug@stormtree.ca>
2023-01-14 17:26:44 -0800

    Legend display and toggle working.

--- src/components/LegendPane.jsx
+++ src/components/LegendPane.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
 import LegendItem from './LegendItem';
 import LegendPatchPolygon from './LegendPatchPolygon';
 import LegendPatchPoints from './LegendPatchPoints';
-
-import { ReactComponent as IconA } from '../data/naturehood/5c311b3537407e1f05ac3946.svg';
+import { useMapLayerStore } from '../data/Questions';
 
 const LegendPane = () => {
+  const layers = useMapLayerStore((state) => state.layers);
+  const legendEntries = [];
+  layers.forEach((layer, key) => {
+    legendEntries.push(
+      <LegendItem key={key} layerId={key}>
+        {layer.format === 'point'
+          ? <LegendPatchPoints pointClasses={layer.propertyMap} />
+          : <LegendPatchPolygon fill={layer.style.fillColor} />
+        }
+      </LegendItem>
+    )
+  });
+
   return (
     <Box w='md' p={2}>
-      <LegendItem name='Layer Name' description='This layer shows the red areas. Data provided by the ministry of red areas.'>
-        <LegendPatchPolygon fill='#ae163e' />
-      </LegendItem>
-      <LegendItem name='Layer Name' description='This layer shows the red areas. Data provided by the ministry of red areas.'>
-        <LegendPatchPoints pointClasses={{
-          'Class 1': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
-          'Class 2': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
-          'Class 3': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
-          'Class 4': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
-        }} />
-      </LegendItem>
+      {legendEntries}
     </Box>
   );
 }
 export default LegendPane;
+
+/*
+<LegendItem name='Layer Name' description='This layer shows the red areas. Data provided by the ministry of red areas.'>
+<LegendPatchPolygon fill='#ae163e' />
+</LegendItem>
+<LegendItem name='Layer Name' description='This layer shows the red areas. Data provided by the ministry of red areas.'>
+<LegendPatchPoints pointClasses={{
+  'Class 1': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
+  'Class 2': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
+  'Class 3': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
+  'Class 4': { icon: (<IconA />), fill: '#4c9f38', stroke: '#4c9f38', legendText: "SDG 3 - Good Health and Well Being" },
+}} />
+</LegendItem>
+*/
--- src/components/MapData.jsx
+++ src/components/MapData.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
-import ReactDOMServer from "react-dom/server";
 import * as L from 'leaflet';
 import { useMap } from 'react-leaflet';
-import Questions from '../data/Questions';
+import { useMapLayerStore } from '../data/Questions';
 
 export const MapData = ({ question }) => {
   const map = useMap();
+  const layers = useMapLayerStore((state) => state.layers);
+  const setQuestionLayersActive = useMapLayerStore(
+    (state) => state.setQuestionLayersActive
+  );
 
   useEffect(() => {
     // Add Satellite Basemap
@@ -20,99 +23,18 @@ export const MapData = ({ question }) => {
       { collapsed: false }
       ).addTo(map);
     */
-
-    const layers = Object.values(Questions.get(question).mapData).map((data) => {
-      var layer = null;
-      var patch = null;
-      if (data.format === 'point') {
-        layer = L.geoJSON(data.data, {
-          pointToLayer: (f,l) => L.marker(l, {
-            icon: featureIconByProperty(f, data.property, data.propertyMap)
-          })
-        });
-        patch = pointLegendPatch(data);
-      } else if (data.format === 'polygon') {
-        layer =  L.geoJSON(data.data, data.style);
-        patch = polygonLegendPatch(data);
-      }
-      return { patch: patch, layer: layer };
-    })
+  
     layers.forEach(el => {
-      map.addLayer(el.layer)
-      //legend.addOverlay(el.layer, el.patch)
+      if (el.active) map.addLayer(el.layer);
     });
     return () => {
       layers.forEach(el => map.removeLayer(el.layer));
-      //map.removeControl(legend);
     };
-  }, [map, question])
+  }, [map, layers]);
+
+  useEffect(() => setQuestionLayersActive(question),
+    [question, setQuestionLayersActive]);
 
   // component renders no elements
   return null;
-}
-
-const featureIconByProperty = (feature, property, propertyMap) => {
-  if (property in feature.properties) {
-    const icon = propertyMap[feature.properties[property]];
-    const marker = (
-      <div style={{
-        borderRadius: '100%',
-        backgroundColor: 'rgba(250,250,250,0.8)',
-        fill: icon?.fill,
-        stroke: icon?.stroke,
-        padding: '3px',
-        width: '32px',
-        height: '32px',
-      }}>{icon?.icon}</div>
-    )
-
-    return L.divIcon({
-      className: "",
-      iconSize: [32, 32],
-      iconAnchor: [12, 12],
-      html: ReactDOMServer.renderToString(marker),
-    });
-  }
-  return;
-}
-
-const polygonLegendPatch = (data) => {
-  return ReactDOMServer.renderToString(
-    <>
-      <div style={{
-        width: '1.5em',
-        height: '1em',
-        background: data.style.fillColor,
-        display: 'inline-block',
-        marginRight: '0.5em',
-        marginTop: '2px'
-      }}>
-      </div>
-      <div style={{ display: 'inline-block' }}>{data.title}</div>
-    </>
-  )
-}
-
-const pointLegendPatch = (data) => {
-  return ReactDOMServer.renderToString(
-    <>
-      <div style={{ display: 'inline-block' }}>{data.title}</div>
-      {Object.keys(data.propertyMap).map(key =>
-        <div key={key} style={{display:'flex', alignItems:'center' }}>
-          <div
-            style={{
-              width: '2em',
-              margin: '4px 0.5em 4px 1.5em',
-              display: 'inline-block',
-              fill: data.propertyMap[key]?.fill,
-              stroke: data.propertyMap[key]?.stroke,
-            }}
-          >{data.propertyMap[key]?.icon}</div>
-          <div style={{ textAlign: 'center', display: 'inline-block' }}>
-            {data.propertyMap[key]?.legendText ?? key}
-          </div>
-        </div>
-      )}
-    </>
-  )
 }
