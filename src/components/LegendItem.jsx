9626fea4f562f5ebdb36914a2c999c1b4d57e618
Doug Johnson <doug@stormtree.ca>
2023-01-14 17:26:44 -0800

    Legend display and toggle working.

--- src/components/LegendItem.jsx
+++ src/components/LegendItem.jsx
@@ -9,21 +9,27 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { FaInfoCircle } from 'react-icons/fa';
+import { useMapLayerStore } from '../data/Questions';
 
-const LegendItem = ({ name, description, children }) => {
+const LegendItem = ({ layerId, children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const layer = useMapLayerStore((state) => state.layers.get(layerId));
+  const toggleLayerActive = useMapLayerStore((state) => state.toggleLayerActive);
 
   return (
     <>
       <Flex direction='row' alignItems='center' gap={2}>
-        <Switch />
-        <FormLabel mb='0' textOverflow='ellipsis'>{name}</FormLabel>
+        <Switch
+          isChecked={layer.active}
+          onChange={(e) => toggleLayerActive(layerId)}
+        />
+        <FormLabel mb='0' textOverflow='ellipsis'>{layer.title}</FormLabel>
         <Spacer />
         {children}
         <IconButton variant='ghost' icon={<FaInfoCircle />} onClick={isOpen ? onClose : onOpen}/>
       </Flex>
       { isOpen
-        ? <LegendItemDetails description={description} children={children} />
+        ? <LegendItemDetails description={"Placeholder."} children={children} />
         : null
       }
     </>
--- src/data/Questions.jsx
+++ src/data/Questions.jsx
@@ -1,5 +1,7 @@
 import produce, { enableMapSet } from 'immer';
 import create from 'zustand';
+import ReactDOMServer from "react-dom/server";
+import * as L from 'leaflet';
 
 import BeatTheHeat from './beattheheat/BeatTheHeat';
 import EatLocal from './eatlocal/EatLocal';
@@ -29,28 +31,122 @@ const questionMap = new Map([
 ]);
 export default questionMap;
 
+// mapData to Leaflet helper function
+const featureIconByProperty = (feature, property, propertyMap) => {
+  if (property in feature.properties) {
+    const icon = propertyMap[feature.properties[property]];
+    const marker = (
+      <div style={{
+        borderRadius: '100%',
+        backgroundColor: 'rgba(250,250,250,0.8)',
+        fill: icon?.fill,
+        stroke: icon?.stroke,
+        padding: '3px',
+        width: '32px',
+        height: '32px',
+      }}>{icon?.icon}</div>
+    )
+
+    return L.divIcon({
+      className: "",
+      iconSize: [32, 32],
+      iconAnchor: [12, 12],
+      html: ReactDOMServer.renderToString(marker),
+    });
+  }
+  return;
+}
+
+// Legend Patch helper functions
+const polygonLegendPatch = (data) => {
+  return ReactDOMServer.renderToString(
+    <>
+      <div style={{
+        width: '1.5em',
+        height: '1em',
+        background: data.style.fillColor,
+        display: 'inline-block',
+        marginRight: '0.5em',
+        marginTop: '2px'
+      }}>
+      </div>
+      <div style={{ display: 'inline-block' }}>{data.title}</div>
+    </>
+  )
+}
+
+const pointLegendPatch = (data) => {
+  return ReactDOMServer.renderToString(
+    <>
+      <div style={{ display: 'inline-block' }}>{data.title}</div>
+      {Object.keys(data.propertyMap).map(key =>
+        <div key={key} style={{display:'flex', alignItems:'center' }}>
+          <div
+            style={{
+              width: '2em',
+              margin: '4px 0.5em 4px 1.5em',
+              display: 'inline-block',
+              fill: data.propertyMap[key]?.fill,
+              stroke: data.propertyMap[key]?.stroke,
+            }}
+          >{data.propertyMap[key]?.icon}</div>
+          <div style={{ textAlign: 'center', display: 'inline-block' }}>
+            {data.propertyMap[key]?.legendText ?? key}
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
 const layerMap = produce(new Map(), draft => {
   questionMap.forEach((q, qKey) => {
-    q.mapData.forEach((layer, index) => {
-      draft.set(qKey + index, { active: true, question: qKey, ...layer });
+    q.mapData.forEach((mapDataLayer, index) => {
+      var mapLayer = null;
+      var legendPatch = null;
+      if (mapDataLayer.format === 'point') {
+        mapLayer = L.geoJSON(mapDataLayer.data, {
+          pointToLayer: (f,l) => L.marker(l, {
+            icon: featureIconByProperty(f, mapDataLayer.property, mapDataLayer.propertyMap)
+          })
+        });
+        legendPatch = pointLegendPatch(mapDataLayer);
+      } else if (mapDataLayer.format === 'polygon') {
+        mapLayer = L.geoJSON(mapDataLayer.data, mapDataLayer.style);
+        legendPatch = polygonLegendPatch(mapDataLayer);
+      }
+      draft.set(qKey + index,
+        { active: true,
+          question: qKey,
+          ...mapDataLayer,
+          layer: mapLayer,
+          patch: legendPatch,
+        });
     });
   });
 });
 
 export const useMapLayerStore = create((set) => ({
   layers: layerMap,
-  filterQuestion: (question) =>
+  setQuestionLayersActive: (question) =>
     set(
       produce((state) => {
-        state.layers = state.layers.forEach((layer) => {
-          if (layer.question !== question) layer.active = false;
+        state.layers.forEach((layer, key) => {
+          layer.active = (layer.question === question);
         })
       })
     ),
-  setLayerState: (layerId, active) =>
+  setLayerActive: (layerId, active = true) =>
     set(
       produce((state) => {
         state.layers.get(layerId).active = active;
       })
     ),
-}))
+  toggleLayerActive: (layerId) =>
+    set(
+      produce((state) => {
+        const layer = state.layers.get(layerId);
+        layer.active = !layer.active;
+      })
+    ),
+}));
