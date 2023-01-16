60535d43c848bdc91e17dd18be15875bcdedc861
Doug Johnson <doug@stormtree.ca>
2023-01-15 16:52:12 -0800

    Gather Legend components and add Drawer

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import {
   useDisclosure,
+  Avatar,
+  AvatarGroup,
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
   FormLabel,
+  Heading,
   IconButton,
   Spacer,
   Switch,
@@ -10,10 +20,82 @@ import {
 } from '@chakra-ui/react';
 import { IoMdInformationCircle, IoMdCloseCircleOutline } from 'react-icons/io';
 import { useMapLayerStore } from '../data/Questions';
-import LegendPatchPolygon from './LegendPatchPolygon';
-import LegendPatchPoints from './LegendPatchPoints';
 
-const LegendItem = ({ layerId }) => {
+export const Legend = ({ activeQuestion }) => {
+  const layers = useMapLayerStore((state) => state.layers);
+  const legendEntries = [];
+  for (const key of layers.keys()) {
+    if (layers.get(key).question === activeQuestion) {
+      legendEntries.push(<LegendItem key={key} layerId={key} mb={1} />);
+    }
+  }
+
+  return (
+    <>
+      {legendEntries}
+    </>
+  )
+}
+
+export const LegendPane = ({ activeQuestion }) => {
+  return (
+    <Box w='lg' p={2} paddingInlineStart={4} overflowY='scroll'>
+      <Heading size='md' align='center' >Legend</Heading>
+      <Legend activeQuestion={activeQuestion} />
+    </Box>
+  )
+}
+
+export const LegendDrawer = ({ activeQuestion, btnRef, isOpen, onClose }) => {
+  return (
+    <Drawer
+      isOpen={isOpen}
+      onClose={onClose}
+      finalFocusRef={btnRef}
+      placement='right'
+      size='sm'
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>
+          <Heading size='md' align='center' >Legend</Heading>
+        </DrawerHeader>
+        <DrawerBody>
+          <Legend activeQuestion={activeQuestion} />
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export const LegendPatchPoints = ({ pointClasses }) => {
+  return (
+    <AvatarGroup size='sm' max={2} spacing='-0.5rem'>
+      {Object.keys(pointClasses).map(key => 
+        <Avatar
+          key={key}
+          icon={pointClasses[key]?.icon}
+          fill={pointClasses[key]?.fill}
+          stroke={pointClasses[key]?.stroke}
+          bg='gray.200'
+        />
+      )}
+    </AvatarGroup>
+  );
+}
+
+export const LegendPatchPolygon = ({ fill }) => {
+  return (
+    <div style={{
+      width: '1.5em',
+      height: '1.25em',
+      background: fill,
+    }} />
+  );
+}
+
+export const LegendItem = ({ layerId }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const layer = useMapLayerStore((state) => state.layers.get(layerId));
   const toggleLayerActive = useMapLayerStore((state) => state.toggleLayerActive);
@@ -49,7 +131,6 @@ const LegendItem = ({ layerId }) => {
     </>
   );
 }
-export default LegendItem;
 
 const LegendItemDetails = ({ description, classes }) => {
   return (
