e1636b9b380a6cf3a014a313af255a70eb11418a
Doug Johnson <doug@stormtree.ca>
2023-01-31 19:48:13 -0800

    Update CarbonBudget color ramp

--- src/data/footprint/Footprint.jsx
+++ src/data/footprint/Footprint.jsx
@@ -23,18 +23,20 @@ const Footprint = {
       description: 'Carbon Budgets for each Municipality in the CRD. Calculated from IPCC SR1.5 report that as of Jan. 1, 2020, divided equally, each person on Earth has a remaining carbon budget of 44.8 tons.',
       data: CRD_Municipal_CarbonBudgets,
       format: 'polygon',
-      style: function (feature) {
-        return {
-          opacity: 0.7,
-          color: getColor(feature.properties.CarbonByArea),
-          dashArray: '',
-          lineCap: 'butt',
-          lineJoin: 'miter',
-          weight: 1.0,
-          fill: true,
-          fillOpacity: 0.7,
-          fillColor: getColor(feature.properties.CarbonByArea),
-          interactive: true
+      options: {
+        style: function (feature) {
+          return {
+            opacity: 0.7,
+            color: getColor(feature.properties.CarbonBudg),
+            dashArray: '',
+            lineCap: 'butt',
+            lineJoin: 'miter',
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 0.7,
+            fillColor: getColor(feature.properties.CarbonBudg),
+            interactive: true
+          }
         }
       }
     },
@@ -43,12 +45,13 @@ const Footprint = {
 export default Footprint;
 
 function getColor(d) {
-  return  d > 73.0790 ? '#800026' :
-          d > 51.5670 ? '#BD0026' :
-          d > 35.7490 ? '#E31A1C' :
-          d > 14.5710 ? '#FC4E2A' :
-          d > 8.5460  ? '#FD8D3C' :
-          d > 2.7210  ? '#FEB24C' :
-          d > 0.9750  ? '#FED976' :
-                        '#FFEDA0';
+  return  d > 1189720 ? '#FFEDA0' :
+          d > 764198  ? '#FED976' :
+          d > 646503  ? '#FEB24C' :
+          d > 513431  ? '#FD8D3C' :
+          d > 470451  ? '#FC4E2A' :
+          d > 216295  ? '#E31A1C' :
+          d > 204345  ? '#BD0026' :
+                        '#800026';
 }
+
