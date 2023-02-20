25e9aa10dda62ea227a90c6f73b62cc5f8a2e36d
Doug Johnson <doug@stormtree.ca>
2023-02-19 21:30:48 -0800

    Legend box-shadow

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -39,7 +39,13 @@ export const Legend = ({ activeQuestion }) => {
 
 export const LegendPane = ({ activeQuestion }) => {
   return (
-    <Box w='lg' p={2} paddingInlineStart={4} overflowY='scroll'>
+    <Box
+      w='lg'
+      p={4}
+      pe={2}
+      overflowY='scroll'
+      boxShadow='inset 0px 11px 8px -10px #CCC, inset 0px -11px 8px -10px #CCC'
+    >
       <Heading size='md' align='center' >Legend</Heading>
       <Legend activeQuestion={activeQuestion} />
     </Box>
