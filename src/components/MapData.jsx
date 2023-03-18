c29a735d631897578197de85ec64cc122ddcda63
Doug Johnson <doug@stormtree.ca>
2023-03-17 21:50:46 -0700

    Fix unused import.

--- src/components/MapData.jsx
+++ src/components/MapData.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Button, useBreakpointValue, useDisclosure } from '@chakra-ui/react';
 import * as L from 'leaflet';
 import { useMap } from 'react-leaflet';
