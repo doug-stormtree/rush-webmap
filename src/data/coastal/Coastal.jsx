d66deaa0db7111a360ec9d209fd3a473b8d4193e
Doug Johnson <doug@stormtree.ca>
2023-03-28 11:36:40 -0700

    Add Unconsolidated

--- src/data/coastal/Coastal.jsx
+++ src/data/coastal/Coastal.jsx
@@ -22,12 +22,12 @@ const styleMap_ShorelineSensitivity = new Map([
   ["5", {legendText: 'very high', color: '#d7191c'}]
 ]);
 
-const styleMap_BED_AS_Sensitivity = new Map([
-  ["1", {legendText: 'very low', color: '#00b547'}],
-  ["2", {legendText: 'low', color: '#81d53e'}],
-  ["3", {legendText: 'moderate', color: '#fdff00'}],
-  ["4", {legendText: 'high', color: '#ffbe00'}],
-  ["5", {legendText: 'very high', color: '#ff0000'}]
+const styleMap_AS_Sensitivity = new Map([
+  ["1", {legendText: 'very low', fillColor: '#00b547'}],
+  ["2", {legendText: 'low', fillColor: '#81d53e'}],
+  ["3", {legendText: 'moderate', fillColor: '#fdff00'}],
+  ["4", {legendText: 'high', fillColor: '#ffbe00'}],
+  ["5", {legendText: 'very high', fillColor: '#ff0000'}]
 ]);
 
 const Coastal = {
@@ -100,7 +100,11 @@ const Coastal = {
       }
     },{
       title: 'Shoreline Sensitivity',
-      description: 'BC Parks developed a model that independently rates marine and terrestrial segments of the British Columbia coastline according to their sensitivity to sea level rise, then spatially combines the ratings to build a map of relative shoreline sensitivity. Ratings were developed using an existing biogeographic land classification dataset (Broad Ecosystem Inventory), and previously rated sensitivity of coastal and marine feature classes (ShoreZone), modified to account for the effects of slope exposure and sediment mobility.',
+      description: [
+        {type: 'p', content:'BC Parks developed a model that independently rates marine and terrestrial segments of the British Columbia coastline according to their sensitivity to sea level rise, then spatially combines the ratings to build a map of relative shoreline sensitivity.'},
+        {type: 'p', content:'Ratings were developed using an existing biogeographic land classification dataset (Broad Ecosystem Inventory), and previously rated sensitivity of coastal and marine feature classes (ShoreZone), modified to account for the effects of slope exposure and sediment mobility.'},
+        {type: 'link', text:'Biffard, D., Stevens, T., Rao, A., and Woods, B., 2014. BC Parks Shoreline Sensitivity Model. Province of British Columbia, Victoria.', url:'https://a100.gov.bc.ca/pub/acat/public/viewReport.do?reportId=42825'},
+      ],
       data: ShorelineSensitivity,
       shape: 'line',
       symbology: 'classified',
@@ -134,12 +138,43 @@ const Coastal = {
         }
       }
     },{
-      title: 'Aquifer Susceptibility',
-      description: '',
-      layer: L.tileLayer(process.env.PUBLIC_URL + 'BED_AS_Tiles/{z}/{x}/{y}.png'),
+      title: 'Aquifer Susceptibility (Bedrock)',
+      description: [
+        {type:'p', content:'An Aquifer is a body of rock or sediment that holds groundwater and readily transmits that water to wells and springs. Aquifer Susceptibility refers to the natural potential for sea water intrusion, not including risk from well pumping or sea level rise, that could lead to increased salinity.'},
+        {type:'p', content:'Bedrock aquifers typically have lower transmissivity to the ocean. This study uses topographic slope and distance from the coast to infer flux, that is, the pressure balance at the boundary between aquifer and ocean.'},
+        {type:'p', content:'Learn more about these models by reading the report here:'},
+        {
+          type:'link',
+          text:'Western Water Associates Ltd (Sivak, T. and M. Wei), 2021. GIS Modelling of Sea Water Intrusion Risk along British Columbia’s Coast. Water Science Series WSS2021-06, Province of British Columbia, Victoria.',
+          url:'https://a100.gov.bc.ca/pub/acat/public/viewReport.do?reportId=59164'
+        }
+      ],
+      layer: L.tileLayer(
+        process.env.PUBLIC_URL + 'BED_AS_Tiles/{z}/{x}/{y}.png',
+        { minNativeZoom: 8, maxNativeZoom: 14, opacity: 0.4 }
+        ),
       shape: 'tile',
       symbology: 'classified',
-      styleMap: styleMap_BED_AS_Sensitivity,
+      styleMap: styleMap_AS_Sensitivity,
+    },{
+      title: 'Aquifer Susceptibility (Unconsolidated)',
+      description: [
+        {type:'p', content:'An Aquifer is a body of rock or sediment that holds groundwater and readily transmits that water to wells and springs. Aquifer Susceptibility refers to the natural potential for sea water intrusion, not including risk from well pumping or sea level rise, that could lead to increased salinity.'},
+        {type:'p', content:'Unconsolidated or sediment-based aquifers typically have much higher transmissivity to the ocean and are found in low-lying areas along the coast. This study uses static groundwater elevation and distance from the coast to infer flux, that is, the pressure balance at the boundary between aquifer and ocean.'},
+        {type:'p', content:'Learn more about these models by reading the report here:'},
+        {
+          type:'link',
+          text:'Western Water Associates Ltd (Sivak, T. and M. Wei), 2021. GIS Modelling of Sea Water Intrusion Risk along British Columbia’s Coast. Water Science Series WSS2021-06, Province of British Columbia, Victoria.',
+          url:'https://a100.gov.bc.ca/pub/acat/public/viewReport.do?reportId=59164'
+        }
+      ],
+      layer: L.tileLayer(
+        process.env.PUBLIC_URL + 'UNC_AS_Tiles/{z}/{x}/{y}.png',
+        { minNativeZoom: 8, maxNativeZoom: 14, opacity: 0.4 }
+        ),
+      shape: 'tile',
+      symbology: 'classified',
+      styleMap: styleMap_AS_Sensitivity,
     }
   ],
 };
