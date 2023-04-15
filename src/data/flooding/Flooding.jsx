765c828fa025d9e907532e415d33a0dd166070d4
Doug Johnson <doug@stormtree.ca>
2023-04-14 17:09:45 -0700

    Update description content

--- src/data/flooding/Flooding.jsx
+++ src/data/flooding/Flooding.jsx
@@ -5,7 +5,8 @@ const Flooding = {
   question: 'How is my community at risk from flooding?',
   image: image,
   description: [
-    {type: 'p', content: "Lorem."},
+    {type: 'p', content: 'Water is life giving.  Our bodies and the surface of the planet are ~70% water.  With so much pavement in the city,  we have limited the areas for water to sink into the ground, be filtered by the plants and soil, and recharge the water table.  Increasing  areas where it can be absorbed by the land (permeable surface) has many advantages including keeping it out of homes.'},
+    {type: 'p', content: 'When water flows across pavement, it carries pollutants and trash into the stormwater system which flows into our streams and creeks and poisons the plants, birds, fish, frogs, and bugs that keep our watersheds beautiful and healthy.'}
   ],
   act: {
     initiatives: [],
