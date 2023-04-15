b5cab3a6003ef52c554baeb84fdd469e83e83ce6
Doug Johnson <doug@stormtree.ca>
2023-04-14 17:09:33 -0700

    Add support for headings to FormattedText

--- src/components/FormattedText.jsx
+++ src/components/FormattedText.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Text } from '@chakra-ui/react';
+import { Heading, Link, Text } from '@chakra-ui/react';
 
 export default function FormattedText({ textArray }) {
   const components = textArray.map((item, index) => {
@@ -10,8 +10,12 @@ export default function FormattedText({ textArray }) {
     switch (item.type) {
       case 'link':
         return <Link {...sharedStyles} isExternal href={item.url ?? "#"}>{item.content}</Link>
+      case 'h1':
+        return <Heading {...sharedStyles}>{item.content}</Heading>
+      case 'h2':
+        return <Heading {...sharedStyles} size='lg'>{item.content}</Heading>
       default:
-        return <Text {...sharedStyles}>{item.content}</Text>;
+        return <Text {...sharedStyles}>{item.content}</Text>
     }
   });
   return <>{components}</>;
