3b27eefc8d7279a452c1db7e37c5b5b325b73ca2
Doug Johnson <doug@stormtree.ca>
2023-04-25 10:18:13 -0700

    Add numbered lists to FormattedText

--- src/components/FormattedText.jsx
+++ src/components/FormattedText.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading, Link, Text } from '@chakra-ui/react';
+import { Heading, Link, Text, VStack } from '@chakra-ui/react';
 
 export default function FormattedText({ textArray }) {
   const components = textArray.map((item, index) => {
@@ -11,9 +11,13 @@ export default function FormattedText({ textArray }) {
       case 'link':
         return <Text><Link {...sharedStyles} isExternal href={item.url ?? "#"}>{item.content}</Link></Text>
       case 'h1':
-        return <Heading {...sharedStyles}>{item.content}</Heading>
-      case 'h2':
         return <Heading {...sharedStyles} size='lg'>{item.content}</Heading>
+      case 'h2':
+        return <Heading {...sharedStyles} size='md'>{item.content}</Heading>
+      case 'list':
+        return <VStack {...sharedStyles} ps='2em' align='left' spacing='0.5em'>{
+            item.content.map((v,i) => <Text key={i}>{`${i+1}. ${v}`}</Text>)
+          }</VStack>
       default:
         return <Text {...sharedStyles}>{item.content}</Text>
     }
