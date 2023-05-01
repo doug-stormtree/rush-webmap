01bc723569a943928f0579aee5b8b5dc795d3e0a
Doug Johnson <doug@stormtree.ca>
2023-05-01 14:57:54 -0700

    Update all Button Images and Initiatives content.

--- src/data/culture/Culture.jsx
+++ src/data/culture/Culture.jsx
@@ -1,14 +1,37 @@
-import image from './image.jpg'
+import image from './CultureCompass.png';
+import WSANECEnvironmentalOrgs from './WSANECEnvironmentalOrgs.png';
+import NativeLandDigital from './NativeLandDigital.png';
+import LittleFreeLibraries from './LittleFreeLibraries.png';
 
 const Culture = {
   title: "Culture Compass?",
   question: "Culture compass?",
   image: image,
   description: [
-    {type: 'p', content: "Lorem."},
+    {type: 'h2', content: "Comparison"},
+    {type: 'p', content: 'Where can we learn about and engage with the cultures of this place? Where are there arts & culture opportunities?'},
   ],
   act: {
-    initiatives: [],
+    initiatives: [
+      {
+        title: 'W̱SÁNEĆ Peninsula Environmental Organizations',
+        image: WSANECEnvironmentalOrgs,
+        link: 'https://storymaps.arcgis.com/stories/5239fadd726c462da56ebcbe231c57be',
+        description: 'There are many people & organizations across the Saanich Peninsula working towards healthier ecosystems and connected communities across municipal boundaries. Learn about their work in this StoryMap created by UVic Community Mapping students Evan Gerbrecht, Casey Lake, Samantha Denton, Ethan Heckrodt with direction from the Saanich Peninsula Environmental Coalition.'
+      },
+      {
+        title: 'Native Land Digital',
+        image: NativeLandDigital,
+        link: 'https://native-land.ca/',
+        description: 'Native Land Digital strives to create and foster conversations about the history of colonialism, Indigenous ways of knowing, and settler-Indigenous relations, through educational resources such as their map and Territory Acknowledgement Guide. They strive to go beyond old ways of talking about Indigenous people and to develop a platform where Indigenous communities can represent themselves and their histories on their own terms.'
+      },
+      {
+        title: 'Little Free Libraries',
+        image: LittleFreeLibraries,
+        link: 'https://victoriaplacemaking.ca/little-free-libraries/',
+        description: 'Little Free Libraries is a popular DIY community concept, where unique homemade book exchange boxes are made freely available in front of homes and businesses for the enjoyment of neighbours and visitors. Learn more and find a map to over 700 of these libraries in the CRD at the Greater Victoria Placemaking Society.'
+      },
+    ],
   },
   mapData: [],
 };
--- src/data/power/Power.jsx
+++ src/data/power/Power.jsx
@@ -1,4 +1,7 @@
-import image from './image.jpg';
+import image from './PowerThisPlace.png';
+import bcSustainableEnergy from './BCSustainableEnergy.png';
+import reBuildInitiative from './ReBuildInitiative.png';
+import greenerHomesGrant from './GreenerHomesGrant.png';
 
 const Power = {
   title: "Power this place?",
@@ -8,7 +11,26 @@ const Power = {
     {type: 'p', content: "Lorem."},
   ],
   act: {
-    initiatives: [],
+    initiatives: [
+      {
+        title: 'BC Sustainable Energy',
+        link: 'https://bcsea.org/',
+        image: bcSustainableEnergy,
+        description: 'BC Sustainable Energy Association is a network of action takers supporting sustainable energy policy, innovation, and education in British Columbia.',
+      },
+      {
+        title: 'The ReBuild Initiative',
+        link: 'https://rebuild.uvic.ca/',
+        image: reBuildInitiative,
+        description: 'Reducing energy use in existing buildings is key to avoiding catastrophic climate change. Better computer models of how our buildings are performing are needed to give robust design solutions and evidence-based policies. Data-driven methods that use machine-learning have great potential as our buildings provide lots of data, but little is currently used for reducing emissions. The ReBuild Initiative is an industry-government-academia consortium that will undertake 16 projects that encompass the breadth and complexity of the challenge, each co-designed with a partner organization to apply specific areas of research to meet their needs.'
+      },
+      {
+        title: 'Greener Homes Grant',
+        link: 'https://www.nrcan.gc.ca/energy-efficiency/homes/canada-greener-homes-initiative/canada-greener-homes-grant/canada-greener-homes-grant/23441',
+        image: greenerHomesGrant,
+        description: 'The Government of Canada wants to help Canadians make where they live more energy efficient. The Canada Greener Homes Initiative will help homeowners save money, create new jobs across Canada for energy advisors and fight climate change.'
+      },
+    ],
   },
   mapData: [],
 };
