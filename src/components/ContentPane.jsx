78dc6f5a2561af3f1734fc9cbfb80798ee6e1a8f
Doug Johnson <doug@stormtree.ca>
2023-05-01 22:39:59 -0700

    Scroll to top button

--- src/components/ContentPane.jsx
+++ src/components/ContentPane.jsx
@@ -4,17 +4,14 @@ import {
   Box,
   Flex,
   Heading,
-  IconButton,
-  Spacer,
   StackDivider,
   VStack
 } from '@chakra-ui/react';
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import Questions from '../data/Questions';
 import ActPane from './ActPane';
 import FormattedText from './FormattedText';
 
-export default function ContentPane({openFlag, setOpenFlag, question, ...props}) {
+export default function ContentPane({question, ...props}) {
   const contentTitle = useBreakpointValue({ lg: Questions.get(question).question, base: Questions.get(question).title})
   return (
     <VStack
@@ -24,32 +21,24 @@ export default function ContentPane({openFlag, setOpenFlag, question, ...props})
       bg='WhiteAlpha.100'
       {...props}
     >
-      <Flex direction='row' w='100%'>
-        <Spacer />
+      <Flex justifyContent='center' w='100%'>
         <Heading size='lg'>{contentTitle}</Heading>
-        <Spacer />
-        <IconButton
-          icon={openFlag ? <FaChevronDown /> : <FaChevronUp />}
-          onClick={setOpenFlag.toggle}
-        />
       </Flex>
-      {openFlag ? (
-        <Flex
-          h='100%'
-          w='100%'
-          gap='10px'
-          direction='column'
-          overflow='auto'
-          alignItems='center'
-        >
-          <Box maxW='6xl'>
-            <Box pb='0.5em'>
-              <FormattedText textArray={Questions.get(question).description} />
-            </Box>
-            <ActPane content={Questions.get(question).act} />
+      <Flex
+        h='100%'
+        w='100%'
+        gap='10px'
+        direction='column'
+        overflow='auto'
+        alignItems='center'
+      >
+        <Box maxW='6xl' px='60px'>
+          <Box pb='0.5em'>
+            <FormattedText textArray={Questions.get(question).description} />
           </Box>
-        </Flex>
-      ) : null}
+          <ActPane content={Questions.get(question).act} />
+        </Box>
+      </Flex>
     </VStack>
   )
 }
