f7dd3a82f713646bd60dfa5d7831291c8cf3ecaf
Doug Johnson <doug@stormtree.ca>
2023-05-11 17:07:53 -0700

    Add hard shorelines data

--- src/data/coastal/Coastal.jsx
+++ src/data/coastal/Coastal.jsx
@@ -9,6 +9,7 @@ import seaLevelRise from './SeaLevelRise.png';
 import surfrider from './Surfrider.png';
 import CRDInundation from './CRDInundation.geojson';
 import ShorelineSensitivity from './ShorelineSensitivity.geojson';
+import HardShorelines from './HardShorelines.geojson'
 
 const styleMap_CRDInundation = new Map([
   ["0",   {legendText: '0.0 m', color: '#1E44D3'}],
@@ -18,11 +19,11 @@ const styleMap_CRDInundation = new Map([
 ]);
 
 const styleMap_ShorelineSensitivity = new Map([
-  ["1", {legendText: 'Very Low', color: '#2b83ba'}],
-  ["2", {legendText: 'Low', color: '#abdda4'}],
-  ["3", {legendText: 'Moderate', color: '#ffffbf'}],
-  ["4", {legendText: 'High', color: '#fdae61'}],
-  ["5", {legendText: 'Very High', color: '#d7191c'}]
+  ["1", {legendText: 'Very Low', color: '#feebe2'}],
+  ["2", {legendText: 'Low', color: '#fbb4b9'}],
+  ["3", {legendText: 'Moderate', color: '#f768a1'}],
+  ["4", {legendText: 'High', color: '#c51b8a'}],
+  ["5", {legendText: 'Very High', color: '#7a0177'}]
 ]);
 
 const styleMap_AquiferSusceptibility = new Map([
@@ -31,6 +32,11 @@ const styleMap_AquiferSusceptibility = new Map([
   ["3", {legendText: 'Moderate', fillColor: '#fdff00'}],
   ["4", {legendText: 'High', fillColor: '#ffbe00'}],
   ["5", {legendText: 'Very High', fillColor: '#ff0000'}]
+]);
+
+const styleMap_HardShorelines = new Map([
+  ['permeable', {legendText: 'Permeable', color: '#bd925a'}],
+  ['impermeable', {legendText: 'Impermeable', color: '#A16928'}],
 ]);
 
 const Coastal = {
@@ -165,7 +171,40 @@ const Coastal = {
             {offset: L.point(0,8)});
         }
       }
-    },{
+    },
+    {
+      title: 'Hard Shorelines',
+      description: [
+        {type: 'p', content: 'Shoreline hard armouring refers to the construction of impermeable concrete seawalls, wooden pilings, or large rock revetments with the goal of preventing erosion from wave energy and sea level rise. Imposing a static configuration on dynamic shoreline systems can diminish salt marsh habitats, valuable ecosystems that support food chains, maintenance of fisheries, water purification, carbon sequestration, coastal erosion, and flood protection.¹'},
+        {type: 'p', content: 'The data shown here is derived from ShoreZone, an international project that takes aerial photography of shorelines and partners with expert biologists and geologists to classify each shore unit. Areas shown on the map represent shorelines that contained over 50 percent human-made structures. Permeable shorelines include rip-rap or wooden crib structures where surface oil from a spill will easily penetrate the structure. Impermeable shorelines include concrete seawalls or steel sheet pile.² This provides some sense of where hard armouring has been constructed but a more accurate inventory should be completed for the Capital Regional District.'},
+        {type: 'link', content: '1. Nicole Peterson, Craig Landry, Alexander Clark, Kevin Samples, and Brian Bledsoe (2019). Socioeconomic and environmental predictors of esuarine shoreline hard armoring. Scientific Reports, 9(1), 16288-10.', url: 'https://doi.org/10.1038/s41598-019-52504-y'},
+        {type: 'link', content: '2. Sarah Cook, Sean Daley, Kalen Morrow, and Sheri Ward (2017). "ShoreZone Coastal Imaging and Habitat Mapping Protocol." Victoria, Canada: Coastal and Ocean Resources.', url: 'https://media.fisheries.noaa.gov/2020-09/ShoreZone-Protocol-2017-AKR.pdf?RSScY5o5Q1RKNuGV2daZiExnYGQzx_Ov'},
+      ],
+      data: HardShorelines,
+      shape: 'line',
+      symbology: 'classified',
+      legendTitle: 'Artificially Constructed Shoreline',
+      styleMap: styleMap_HardShorelines,
+      options: {
+        style: function (feature) {
+          const baseStyle = {
+            stroke: true,
+            fill: false,
+            interactive: true,
+            weight: 4,
+          }
+          return {
+            ...baseStyle,
+            color: getStyleMapProperty(
+              'color',
+              feature.properties.Permeability,
+              styleMap_HardShorelines
+            )
+          }
+        }
+      }
+    },
+    {
       title: 'Sea to Groundwater Risk (Bedrock)',
       description: [
         {type:'p', content:'An Aquifer is a body of rock or sediment that holds groundwater and readily transmits that water to wells and springs. Aquifer Susceptibility refers to the natural potential for sea water intrusion, not including risk from well pumping or sea level rise, that could lead to increased salinity.'},
