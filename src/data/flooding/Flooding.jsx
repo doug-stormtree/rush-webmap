42cb65938d5c7becc39598829ddcd244f633663c
Doug Johnson <doug@stormtree.ca>
2023-05-15 20:19:18 -0700

    Add watersheds layer to Flooding

--- src/data/flooding/Flooding.jsx
+++ src/data/flooding/Flooding.jsx
@@ -1,8 +1,9 @@
-import * as L from 'leaflet';
+import { icon, marker, point } from 'leaflet';
 import image from './ProtectFromFlooding.png';
 import rainGardenStoryMapImage from './RainGardenStoryMap.png';
 import watershedsBC from './WatershedsBC.png';
 import crdWatershedsMap from './CRDWatershedMap.png';
+import Watersheds from './CRDWatersheds.geojson';
 import RainGardens from './1000RainGardens.geojson';
 import { ReactComponent as WaterIcon } from '../beattheheat/water.svg';
 import { mapPopupContent } from '../LeafletStyleHelpers';
@@ -61,6 +62,47 @@ const Flooding = {
   },
   mapData: [
     {
+      title: 'Watersheds',
+      description: [
+        {type: 'p', content: 'A watershed (also known as a hydrologic unit, catchment, or drainage basin) is defined as that area of land draining into a particular stream or other surface water body. For any location in a stream, there is an associated area that contributes water to its flow. The watershed divide is that line which divides the area contributing water to the stream and that which contributes water to neighboring streams or water bodies. Therefore, each watershed is defined by its outlet or pour point (the point in the stream which receives all water in the watershed) and the associated watershed divide derived from that point and the local topography.¹'},
+        {type: 'p', content: 'If a drop of rain or snow falls on any spot within the boundary of a watershed, if it is not absorbed into the ground or somehow otherwise collected, it can be expected to eventually make its way to the central stream and flow along it to the sea. Even water that soaks into the ground can flow towards the stream as it reaches denser soils or bedrock that it cannot infiltrate. Some water travels much deeper into underground resevoirs, called aquifers, that provide drinking water when accessed by a well.²'},
+        {type: 'link', content: '1. GeoBC, 2010. "Freshwater Atlas User Guide." Integrated Land Management Bureau, Victoria, BC. ', url: 'https://www2.gov.bc.ca/assets/gov/data/geographic/topography/fwa/fwa_user_guide.pdf'},
+        {type: 'link', content: '2. NOAA. "What is a watershed?" Accessed: May 15, 2023.', url: 'https://oceanservice.noaa.gov/facts/watershed.html'},
+      ],
+      data: Watersheds,
+      shape: 'polygon',
+      symbology: 'single',
+      options: {
+        style: {
+          stroke: true,
+          opacity: 0.7,
+          color: 'rgb(28,163,236)',
+          dashArray: '',
+          lineCap: 'butt',
+          lineJoin: 'miter',
+          weight: 3,
+          fill: true,
+          fillOpacity: 0,
+          fillColor: 'rgb(132,182,185)',
+          interactive: true,
+        },
+        onEachFeature: (f,l) => {
+          l.bindPopup(
+            mapPopupContent(
+              f.properties.Name.charAt(0) === '9' ? 'Watershed' : f.properties.Name + ' Watershed',
+              f.properties.Name.charAt(0) === '9'
+                ? `This watershed is not associated with a named stream by GeoBC. Its Freshwater Atlas Code is ${f.properties.Name}. Its area is ${f.properties.Hectares} hectares.`
+                : f.properties.Name + ' Watershed encloses an area of ' + f.properties.Hectares + ' hectares.',
+              ),
+            {offset: point(0,8)});
+          l.on({
+            mouseover: (e) => e.target.setStyle({ fillOpacity: 0.7 }),
+            mouseout: (e) => e.target.setStyle({ fillOpacity: 0 })
+          });
+        }
+      }
+    },
+    {
       title: '1000 Rain Gardens',
       description: [
         {type: 'p', content: "This is the map for the Friends of Bowker Creek and Peninsula Streams Society '1000 Rain Gardens' mapping project!"},
@@ -72,12 +114,12 @@ const Flooding = {
       styleMap: styleMap_RainGardens,
       options: {
         pointToLayer: (f,l) => {
-          const icon = L.icon({
+          const ogmIcon = icon({
             iconUrl: `https://new.opengreenmap.org/api-v1/icons/${f.properties.icons[0]}/image/value`,
             iconSize: [48,48],
           })
 
-          return f.properties?.icons[0] ? L.marker(l, {icon: icon}) : null;
+          return f.properties?.icons[0] ? marker(l, {icon: ogmIcon}) : null;
         },
         onEachFeature: (f,l) => {
           const imageURL = f.properties.pictures[0]
@@ -90,10 +132,11 @@ const Flooding = {
               `https://https://new.opengreenmap.org/browse/sites/${f.properties._id}`,
               'Show More at OpenGreenMap.org',
               imageURL
-            ), {offset: L.point(4,2)});
+            ), {offset: point(4,2)});
         }
       },
-    }
+    },
+
   ],
 };
 export default Flooding;
