60242a8d73b3780ccd8fe64e6811684dd02e9451
Doug Johnson <doug@stormtree.ca>
2023-05-15 21:24:32 -0700

    Update Descriptions

--- src/data/culture/Culture.jsx
+++ src/data/culture/Culture.jsx
@@ -9,11 +9,14 @@ const Culture = {
   image: image,
   description: [
     {type: 'h2', content: "Comparison"},
-    {type: 'p', content: 'Connection between people and places. Public art, walking zones, public parks, fountains, squares, maker spaces, art galleries, commemorative art markers, community centers, co-working spaces, repurposed abondoned or empty spaces, community gardens, walking tours.'},
+    {type: 'p', content: 'Free and accessible places and ways to get a sense of community and belonging. Public art, walking zones, public parks, fountains, squares, maker spaces, art galleries, commemorative art markers, community centers, co-working spaces, repurposed abondoned or empty spaces, community gardens, walking tours.'},
     {type: 'h2', content: "Problem"},
-    {type: 'p', content: 'Having something to do, places to go, people to see within walking distance.'},
+    {type: 'p', content: 'The UN Declaration on the Rights of Indigenous Peoples is only recently being applied by governments in Canada to show more respect, care, and recognition of the autonomy and sovereignty of Indigenous communities. Hardly a century ago European settlers empowered by their governments took over lands and resources in BC and tried to eliminate the original people and culture of this place. Healing from these extreme acts of oppression does not happen easily or quickly without practical changes in the way our governments,  communities and individuals live and work together.'},
+    {type: 'p', content: 'Global problems like climate change and war are forcing people to abandon their homes and families to seek better opportunities but risking a sense of belonging in the process. Creating a culture of reconciliation with Indigenous communities and a warmth of welcome for people fleeing other forms of oppression is crucial.'},
     {type: 'h2', content: "Solution"},
-    {type: 'p', content: 'Place-making is an international movement. Gathering spaces. Community mapping. Moblity, safety, intergenerational.'},
+    {type: 'p', content: 'The Truth and Reconciliation Commission of Canada created 94 calls to action to guide meaningful steps towards reconciliation. Respecting days of observance (Red Dress Day - MMIWG2S - May 5, National Indigenous Peoples Day - June 21,  National Truth and Reconciliation Day - September 30), Indigenous leadership, knowledge and stewardship of this land opens a way of understanding how to work with Nature here in a way that is reciprocal and creates abundance.  It is not only about cultural appreciation but honouring rights and bringing integrity to working together.'},
+    {type: 'link', content: 'According to Gallup, a true culture of belonging happens when people appreciate what you bring to the community, people seek meaningful relationships and, differences between people are appreciated.', url: 'https://www.gallup.com/workplace/395102/drives-culture-belonging.aspx'},
+    {type: 'p', content: 'Place-making is an international movement to cultivate a sense of belonging and strength in communities. Gathering spaces like fountains and skate parks foster community connection when they are a safe and accessible walk, roll, bike or bus ride from home. The Greater Victoria Place-making Network and local community organizations are good ways to  create the vibe and features you want to see in your neighbourhood.'}
   ],
   act: {
     initiatives: [
--- src/data/power/Power.jsx
+++ src/data/power/Power.jsx
@@ -5,10 +5,24 @@ import greenerHomesGrant from './GreenerHomesGrant.png';
 
 const Power = {
   title: "Power this place?",
-  question: "Power this place?",
+  question: "How to plan for local and sustainable energy in the long run?",
   image: image,
   description: [
-    {type: 'p', content: "Lorem."},
+    {type: 'h2', content: 'Comparison'},
+    {type: 'p', content: 'Distances that power is transported.  GHG emissions CRD buidlings & industry.  Where are the opportunities for off grid.'},
+    {type: 'h2', content: 'Problem'},
+    {type: 'p', content: 'Problem: Much of BC’s energy is generated through hydro power, we sell some power to the US at a high price and buy it back at a lower price. This dynamic increases the amount of energy that is regularly lost in transmission and distribution across distances powerlines - estimated to be between 10-20%.¹𝄒²'},
+    {type: 'p', content: 'Natural gas is our second biggest source and comes with a host of expensive and risky extraction practices.  Research is warning that gas stoves and fireplaces are not good for indoor air quality so it is probably safe to assume that it is not enhancing outdoor air quality either. 90% of the costs associated with the liquified natural gas value chain occur up front so, as a province, we then commit to long term contracts in shipping it all over the world to make good on the initial investment risk.³ It can seem like choices about power are outside the locus of our control but our population on Vancouver Island means that we are one of the largest customers of energy generated in BC so our consumption behaviours influence on how the whole system operates.'},
+    {type: 'h2', content: 'Solution'},
+    {type: 'p', content: 'Ideally our energy would be renewable and generated close to the source. Vancouver Island has a significant and growing number of alternative energy providers as the demand for local and renewable energy increases. T’Souke operates BC’s largest First Nation owned solar company.'},
+    {type: 'p', content: 'National and provincial government programs to increase the efficiency of buildings are plentiful and BC is a hotbed of innovation and talent when it comes to green developments. In 2019, GHG emissions from buildings, facilities, and infrastructure have decreased by 21% since 2007, despite adding new buildings and facilities.⁴'},
+    {type: 'p', content: 'Integrated Resource Recovery is a name for developments that co-locate buildings and businesses that can use each other’s waste heat , materials & energy. Some municipalities around the world have created local biogas fuel sources that are owned and operated by the local government creating a level of autonomy and sustainability that is impressive and practical.⁵'},
+
+    {type: 'link', content: '1. "Power in high voltage lines: lost in transmission," Smart Energy International. Accessed May 15, 2023.', url: 'https://www.smart-energy.com/partners/power-in-high-voltage-lines-lost-in-transmission/'},
+    {type: 'link', content: '2. Jordan Wirfs-Brock, "Lost In Transmission: How Much Electricity Disappears Between A Power Plant And Your Plug?" Inside Energy, November 6, 2015.', url: 'https://insideenergy.org/2015/11/06/lost-in-transmission-how-much-electricity-disappears-between-a-power-plant-and-your-plug/'},
+    {type: 'link', content: '3. Gergely Molnar, 2022. Economics of Gas Transportation by Pipeline and LNG. In: Hafner, M., Luciani, G. (eds) The Palgrave Handbook of International Energy Economics. Palgrave Macmillan, Cham.', url: 'https://doi.org/10.1007/978-3-030-86884-0_2'},
+    {type: 'link', content: '4. "Reducing Greenhouse Gas Emissions - Buildings," Capital Regional District. Accessed May 15, 2023.', url: 'https://www.crd.bc.ca/education/climate-action/reducing-greenhouse-gas-emissions/buildings'},
+    {type: 'link', content: '5. "Karpalund Biogas Plant, Kristianstad," UN Climate Technology Centre & Network. Accessed May 15, 2023.', url: 'https://www.ctc-n.org/products/karpalund-biogas-plant-kristianstad'},
   ],
   act: {
     initiatives: [
