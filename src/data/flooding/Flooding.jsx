b5268b042f036ac1ca5f4ad2289baf990e3ea1cc
Doug Johnson <doug@stormtree.ca>
2023-05-23 09:57:32 -0700

    Add Coastal Inundation to Flooding

--- src/data/flooding/Flooding.jsx
+++ src/data/flooding/Flooding.jsx
@@ -6,7 +6,8 @@ import crdWatershedsMap from './CRDWatershedMap.png';
 import Watersheds from './CRDWatersheds.geojson';
 import RainGardens from './1000RainGardens.geojson';
 import RestorationSites from './RestorationSites.geojson';
-import { mapPopupContent, pointToIcon } from '../LeafletStyleHelpers';
+import CRDInundation from '../coastal/CRDInundation.geojson';
+import { getStyleMapProperty, mapPopupContent, pointToIcon } from '../LeafletStyleHelpers';
 
 const styleMap_RainGardens = new Map([
   ['641b6ff089e06d0100e0cf1d', {
@@ -21,7 +22,7 @@ const styleMap_RainGardens = new Map([
     src: `https://new.opengreenmap.org/api-v1/icons/641b714889e06d0100e0cf24/image/value`,
     legendText: 'Curb Cut'
   }],
-])
+]);
 
 const styleMap_RestorationSites = new Map([
   ['5ebee86cd060db010045ee3c', {
@@ -60,7 +61,14 @@ const styleMap_RestorationSites = new Map([
     src: `https://new.opengreenmap.org/api-v1/icons/5ca7bfececd8490100cab9fc/image/value`,
     legendText: 'Canoe or Kayak Site'
   }],
-])
+]);
+
+const styleMap_CRDInundation = new Map([
+  ["0",   {legendText: '0.0 m', color: '#1E44D3'}],
+  ["0.5", {legendText: '0.5 m', color: '#5177E1'}],
+  ["1",   {legendText: '1.0 m', color: '#81A4EE'}],
+  ["2",   {legendText: '2.0 m', color: '#C6DBFA'}]
+]);
 
 const Flooding = {
   title: 'Protect from Flooding?',
@@ -144,6 +152,43 @@ const Flooding = {
             mouseover: (e) => e.target.setStyle({ fillOpacity: 0.7 }),
             mouseout: (e) => e.target.setStyle({ fillOpacity: 0 })
           });
+        }
+      }
+    },
+    {
+      title: 'Coastal Flood Inundation Scenarios',
+      description: [
+        {type: 'p', content: 'The Capital Regional District (CRD) retained Associated Engineering, DHI and Westmar Advisors to undertake the Capital Region Coastal Flood Inundation Mapping Project (the project) in 2019 -2021. On behalf of local government and other regional stakeholders, staff worked closely with an inter-municipal and multi-disciplinary project team to complete the Regional Coastal Flood Inundation Project in order to better understand regional impacts from coastal storm flooding due to sea level rise and tsunamis. Modeled scenarios show the higher high water large tide (HHWLT) levels for a 0.0m, 0.5m, 1.0m, and 2.0m relative sea level rise (RSLR), respectively.'},
+        {type: 'link', content: 'Capital Regional District, 2021. Capital Region Coastal Flood Inundation Mapping Project Summary, Version 2.0. Victoria.', url: 'https://www.crd.bc.ca/docs/default-source/climate-action-pdf/reports/2020-sea-level-mapping-project/coastal-flood-inundation-mapping-project-summary.pdf'},
+      ],
+      data: CRDInundation,
+      shape: 'line',
+      symbology: 'classified',
+      styleMap: styleMap_CRDInundation,
+      options: {
+        style: function (feature) {
+          const baseStyle = {
+            stroke: true,
+            fill: false,
+            interactive: true,
+            weight: 4,
+          }
+          return {
+            ...baseStyle,
+            color: getStyleMapProperty(
+              'color',
+              feature.properties.RSLR,
+              styleMap_CRDInundation
+            ),
+          }
+        },
+        onEachFeature: (f,l) => {
+          l.bindPopup(
+            mapPopupContent(
+              'Capital Region Coastal Flood Inundation',
+              'Scenario of ' + f.properties.RSLR + 'm Relative Sea Level Rise (RSLR), this line represents the Higher High Water Large Tide (HHWLT) level.',
+              ),
+            {offset: point(0,8)});
         }
       }
     },
