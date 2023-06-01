c1155bc90735a64626fe088d74e5d65da6dc7e24
Doug Johnson <doug@stormtree.ca>
2023-05-31 19:05:28 -0700

    Bold NavBar Text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ import {
   Link,
   Spacer,
   Text,
+  useBreakpointValue,
   useStyleConfig,
 } from '@chakra-ui/react';
 import { FiMessageSquare } from 'react-icons/fi';
@@ -16,9 +17,14 @@ export default function NavBar(props) {
   const styles = useStyleConfig('NavBar');
   const { getShareURL, ...rest } = props;
 
+  const pageTitle = useBreakpointValue({
+    xl: '[RUSH] Resilient Urban Systems & Habitat',
+    base: '[RUSH]',
+  },{ssr:false, fallback:true});
+
   return (
     <Box __css={styles} {...rest} >
-      <Text size="xl" m="auto 1em">RUSH</Text>
+      <Text size="xl" m="auto 1em" fontWeight='medium'>{pageTitle}</Text>
       <Spacer />
       <ButtonGroup variant='nav' spacing='-2'>
         <Link 
