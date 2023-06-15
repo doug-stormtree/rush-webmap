0c76dd97286a6dfa8a932b961deecd66478b69a5
Doug Johnson <doug@stormtree.ca>
2023-06-15 11:22:50 -0700

    Fix layer property name errors

--- src/data/layers/EVChargingStations.jsx
+++ src/data/layers/EVChargingStations.jsx
@@ -16,7 +16,7 @@ const layer = {
     {type: 'link', content: 'View detailed charging station information, contribute to the map, and learn more about the project here.', url: 'https://openchargemap.org/site'}
   ],
   legendTitle: 'Charging Level (Speed)',
-  layer: L.layerJSON({
+  leafletLayer: L.layerJSON({
     url: "https://api.openchargemap.io/v3/poi/?output=geojson&countrycode=CA&usagetypeid=1&statustypeid=50&maxresults=3000&compact=false&camelcase=true&verbose=false&includecomments=true&key=ca66f44f-646d-4f64-98ec-639b924d2839&boundingbox=({lat1}%2C{lon1})%2C({lat2}%2C{lon2})",
     propertyItems: 'features',
     propertyTitle: 'properties.name',
