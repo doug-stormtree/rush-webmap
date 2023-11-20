69d3de6584a3e588f59c1524ad1a77181c015e86
Doug Johnson <doug@stormtree.ca>
2023-11-19 22:12:30 -0800

    Change to client-side session management

--- src/components/MapBasemap.jsx
+++ src/components/MapBasemap.jsx
@@ -3,72 +3,47 @@ import * as L from 'leaflet';
 import { useMap } from 'react-leaflet';
 import Control from 'react-leaflet-custom-control';
 
-const basemap = L.tileLayer(
-  "https://rush-basemap.dxdjohn.workers.dev/tile/{z}/{x}/{y}",
-  {
-    minZoom: 0,
-    maxZoom: 22,
-    updateWhenIdle: true,
-    zIndex: 0
-  }
-)
-
-const attrMap = new Map();
-const ATTR_LOADING = 'Loading...'
-
-const mapToBoundsZoomString = (t) => {
-  const b = t.getBounds()
-  const z = t.getZoom()
-  return String(z).concat(
-    '/',
-    b.getNorth().toFixed(3),
-    '/',
-    b.getSouth().toFixed(3),
-    '/',
-    b.getEast().toFixed(3),
-    '/',
-    b.getWest().toFixed(3)
-  )
+// Constants
+const SESSION_STORAGE_KEY = 'gmapSession'
+const API_URL = "https://rush-basemap.dxdjohn.workers.dev/"
+const API_SESSION_PATH = "createSession"
+const API_SESSION_OPTS = {
+  mapType: 'satellite',
+  language: 'en-CA',
+  region: 'CA',
 }
-
-const fetchAttribution = (view, setAttr) => {
-  if (attrMap.has(view)) {
-    const attr = attrMap.get(view)
-    if (attr === ATTR_LOADING) return
-    setAttr(attr)
-  } else {
-    console.log('Fetching attr: ' + view)
-    attrMap.set(view, ATTR_LOADING) // Set loading to prevent duplicate fetches
-    const attrURL = 'https://rush-basemap.dxdjohn.workers.dev/attribution/'
-      .concat(view)
-    fetch(attrURL)
-      .then(res => res.json())
-      .then(res => {
-        attrMap.set(view, res.attribution) // Cache response
-        setAttr(res.attribution)
-      })
-  }
+const API_TILE_PATH = "2dtiles/{z}/{x}/{y}"
+const API_ATTR_PATH = "viewport"
+const TILE_LAYER_OPTS = {
+  minZoom: 0,
+  maxZoom: 22,
+  updateWhenIdle: true,
+  zIndex: 0
 }
-
-const ukrainianFlag = <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8"><path fill="#4C7BE1" d="M0 0h12v4H0z"/><path fill="#FFD500" d="M0 4h12v3H0z"/><path fill="#E0BC00" d="M0 7h12v1H0z"/></svg>
+const UKRAINIAN_FLAG = <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8"><path fill="#4C7BE1" d="M0 0h12v4H0z"/><path fill="#FFD500" d="M0 4h12v3H0z"/><path fill="#E0BC00" d="M0 7h12v1H0z"/></svg>
 
 export const MapBasemap = () => {
-  const map = useMap();
+  const map = useMap()
+  const session = useGoogleBasemapSession()
 
   useEffect(() => {
-    if (map === undefined) return;
+    if (map === undefined || session?.token === undefined) return;
 
     // Add Satellite Basemap
-    if (!map.hasLayer(basemap)) map.addLayer(basemap);
+    const basemap = L.tileLayer(
+      API_URL + API_TILE_PATH + `?session=${session.token}`,
+      TILE_LAYER_OPTS
+    )
+    map.addLayer(basemap);
 
     return () => {
       map.removeLayer(basemap)
     };
-  }, [map]);
+  }, [map, session]);
 
   return (
     <>
-      <AttributionControl />
+      <AttributionControl session={session} />
       <Control position='bottomleft'>
         <img
           src='/google_on_non_white.png'
@@ -84,72 +59,177 @@ export const MapBasemap = () => {
 }
 export default MapBasemap
 
-const AttributionControl = () => {
-  const map = useMap();
+function useGoogleBasemapSession() {
+  const [session, setSession] = useState(undefined)
+
+  // populate session
+  useEffect(() => {
+    let ignore = false
+    
+    function requestSession() {
+      // request new session from api
+      const sessionRequest = new Request(
+        API_URL + API_SESSION_PATH,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(API_SESSION_OPTS),
+        }
+      )
+      fetch(sessionRequest)
+        .then((res) => res.json())
+        .then((res) => {
+          if (!ignore && res.session && res.expiry) {
+            const newSession = { token: res.session, expiry: res.expiry }
+            localStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(newSession))
+            setSession(newSession)
+          }
+        })
+    }
+
+    function willExpireIn(expiry, minutes) {
+      return (expiry * 1000) < (Date.now() + (minutes * 60000))
+    }
+
+    // check if session has been populated
+    if (session === undefined) {
+      // check local storage for an unexpired previous session
+      const localSession = JSON.parse(localStorage.getItem(SESSION_STORAGE_KEY))
+      if (localSession && !willExpireIn(localSession.expiry, 120)) {
+        setSession(localSession)
+      } else {
+        // request a new session from api
+        requestSession()
+      }
+    } else if (willExpireIn(session.expiry, 120)) {
+      setSession(undefined)
+    }
+
+    return () => { ignore = true }
+  }, [session])
+
+  return session
+}
+
+//----
+const attrMap = new Map();
+const ATTR_LOADING = 'Loading...'
+const mapToView = (map) => {
+  const b = map.getBounds()
+  const z = String(map.getZoom())
+  const view = {
+    z: z,
+    n: b.getNorth().toFixed(3),
+    s: b.getSouth().toFixed(3),
+    e: b.getEast().toFixed(3),
+    w: b.getWest().toFixed(3),
+  }
+  view.key = `${view.z}${view.n}${view.s}${view.e}${view.w}`
+  return view
+}
+const fetchViewAttr = (view, session, setAttr, stale = false) => {
+    if (attrMap.has(view.key)) {
+      const attr = attrMap.get(view.key)
+      if (attr === ATTR_LOADING) return
+      setAttr(attr)
+    } else {
+      console.log('Fetching attr: ' + view.key)
+      // Set loading to prevent duplicate fetches
+      attrMap.set(view.key, ATTR_LOADING)
+
+      // Fetch attribution
+      const attrParams = new URLSearchParams({
+        session: session.token,
+        zoom: view.z,
+        north: view.n,
+        south: view.s,
+        east: view.e,
+        west: view.w
+      })
+      const attrURL = API_URL.concat(API_ATTR_PATH, '?', attrParams)
+      fetch(attrURL)
+        .then(res => res.json())
+        .then(res => {
+          attrMap.set(view, res.copyright) // Cache response
+          console.log(stale)
+          if (!stale) setAttr(res.copyright)
+        })
+    }
+  }
+
+const AttributionControl = ({ session }) => {
+  const map = useMap()
   const [attr, setAttr] = useState(undefined)
   const [isFocused, setFocus] = useState(false)
 
   useEffect(() => {
-    if (map === undefined || setAttr === undefined) return;
+    if (map === undefined || session?.token === undefined) return;
+    
 
-    // Add Basemap Update Attribution Listener 
+    // Add Basemap Update Attribution Listener
+    let stale = false
     const eventToAttribution = (e) => {
-      fetchAttribution(
-        mapToBoundsZoomString(e.target),
-        setAttr
-      )
+      const view = mapToView(e.target)
+      fetchViewAttr(view, session, setAttr, stale)
     }
     map.on('moveend zoomend', eventToAttribution)
-
+    
     // Remove Attribution Control
     map.removeControl(map.attributionControl)
 
     return () => {
+      stale = true
       map.off('moveend zoomend', eventToAttribution)
     };
-  }, [map, setAttr]);
-
+  }, [map, session, attr]);
+  
   useEffect(() => {
-    if (map === undefined || attr !== undefined) return;
-    fetchAttribution(
-      mapToBoundsZoomString(map),
-      setAttr
-    )
-  }, [map, attr])
-
+    if (
+      map === undefined ||
+      attr !== undefined ||
+      session?.token === undefined
+    ) return;
+    // fetch attribtion for current map view
+    let stale = false
+    fetchViewAttr(mapToView(map), session, setAttr, stale)
+    return () => { stale = true }
+  }, [map, attr, session])
+  
   return (
     <Control position='bottomright'>
-      <div
-        onMouseEnter={() => setFocus(true)}
-        onMouseLeave={() => setFocus(false)}
-        style={{
-          backgroundColor: '#FFFFFFB3',
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          margin: '0 -10px -10px 0',
-          padding: '0.2em',
-          gap: '0.4em',
-          maxWidth: '80vw',
-          whiteSpace: isFocused ? 'normal' :'nowrap',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-        }}
+    <div
+      onMouseEnter={() => setFocus(true)}
+      onMouseLeave={() => setFocus(false)}
+      style={{
+        backgroundColor: '#FFFFFFB3',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: '0 -10px -10px 0',
+        padding: '0.2em',
+        gap: '0.4em',
+        maxWidth: '80vw',
+        whiteSpace: isFocused ? 'normal' :'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+      }}
       >
-        <div>
-          <a href="https://leafletjs.com" title="A JavaScript library for interactive maps">
-            <div style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: '0.2em',
-            }}>
-              {ukrainianFlag}
-              Leaflet
-            </div>
-          </a>
-        </div>
-        <div aria-hidden='true'>|</div>
+      <div>
+        <a href="https://leafletjs.com" title="A JavaScript library for interactive maps">
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: '0.2em',
+          }}>
+            {UKRAINIAN_FLAG}
+            Leaflet
+          </div>
+        </a>
+      </div>
+      <div aria-hidden='true'>|</div>
         Basemap: Google{attr ? ', ' + attr : ''}
       </div>
     </Control>
