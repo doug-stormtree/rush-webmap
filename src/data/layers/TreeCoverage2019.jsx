d5c95b247fd9a0937dbf92f1260b9afd815e4985
Doug Johnson <43759986+doug-stormtree@users.noreply.github.com>
2024-02-15 01:27:21 +0000

    Add Tree Coverage 2019

---
+++ src/data/layers/TreeCoverage2019.jsx
@@ -0,0 +1,39 @@
+import { tileLayer } from 'leaflet';
+import { StartOffLayersName, StartOnLayersName } from '../TextContent';
+
+const styleMap = new Map([
+  ["1", {legendText:   '0 - 10',  fillColor: 'rgb(205, 205, 205)'}],
+  ["2", {legendText: '>10 - 20',  fillColor: 'rgb(239, 240, 201)'}],
+  ["3", {legendText: '>20 - 30',  fillColor: 'rgb(246, 253, 148)'}],
+  ["4", {legendText: '>30 - 40',  fillColor: 'rgb(92, 183, 105)'}],
+  ["5", {legendText: '>40 - 75',  fillColor: 'rgb(39, 151, 78)'}],
+  ["6", {legendText: '>75 - 100', fillColor: 'rgb(0, 104, 55)'}],
+]);
+
+const layer = {
+  title: 'Tree Coverage (2019)',
+  description: [
+    {type:'p', content:'Estimated 2005 Tree Cover Percentage. The data have been created on behalf of the Urban Forest Stewardship Initiative (UFSI), a program of Habitat Acquisition Trust (HAT) by Caslys Consulting Ltd.'},
+    {type:'p', content:'Learn more and compare with other years by reading the report here:'},
+    {
+      type:'link',
+      content:'Caslys Consulting Ltd., 2013. Capital Regional District Land Cover Mapping 1986, 2005 and 2011 Summary Report. Capital Regional District, Victoria.',
+      url:'https://hat.bc.ca/s/CRD_2011_land_cover_EXECUTIVE_summary.pdf'
+    }
+  ],
+  leafletLayer: tileLayer(
+    '/CanopyCover19/{z}/{x}/{y}.png',
+    { minNativeZoom: 14, maxNativeZoom: 14, opacity: 0.5 }
+    ),
+  shape: 'tile',
+  symbology: 'classified',
+  legendTitle: 'Percentage of Tree Cover',
+  styleMap: styleMap,
+  questions: [
+    { key: 'beat-the-heat', group: StartOnLayersName, active: true },
+    { key: 'neighbourhood-to-naturehood', group: StartOffLayersName, active: false },
+    { key: 'development', group: 'Greenspace', active: false },
+  ]
+}
+
+export default layer;
