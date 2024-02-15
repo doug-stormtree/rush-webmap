458650860a8debcb59df350b06a2a025d3aae273
Doug Johnson <43759986+doug-stormtree@users.noreply.github.com>
2024-02-15 02:29:56 +0000

    Update TreeCoverage2005 Legend Colors

--- src/data/layers/TreeCoverage2005.jsx
+++ src/data/layers/TreeCoverage2005.jsx
@@ -1,19 +1,20 @@
 import { tileLayer } from 'leaflet';
 import { StartOffLayersName, StartOnLayersName } from '../TextContent';
 
-const styleMap_TreeCoverage2005 = new Map([
-  ["1", {legendText: '0 - 5', fillColor: 'rgb(200, 38, 0)'}],
-  ["2", {legendText: '>5 - 10', fillColor: 'rgb(156, 70, 0)'}],
-  ["3", {legendText: '>10 - 25', fillColor: 'rgb(112, 102, 0)'}],
-  ["4", {legendText: '>25 - 50', fillColor: 'rgb(79, 101, 5)'}],
-  ["5", {legendText: '>50 - 75', fillColor: 'rgb(39, 84, 16)'}],
-  ["6", {legendText: '>75 - 100', fillColor: 'rgb(0, 68, 27)'}],
+const styleMap = new Map([
+  ["1", {legendText:   '0 - 10',  fillColor: 'rgb(205, 205, 205)'}],
+  ["2", {legendText: '>10 - 20',  fillColor: 'rgb(239, 240, 201)'}],
+  ["3", {legendText: '>20 - 30',  fillColor: 'rgb(246, 253, 148)'}],
+  ["4", {legendText: '>30 - 40',  fillColor: 'rgb(92, 183, 105)'}],
+  ["5", {legendText: '>40 - 75',  fillColor: 'rgb(39, 151, 78)'}],
+  ["6", {legendText: '>75 - 100', fillColor: 'rgb(0, 104, 55)'}],
 ]);
 
 const layer = {
   title: 'Tree Coverage (2005)',
   description: [
     {type:'p', content:'Estimated 2005 Tree Cover Percentage. The data have been created on behalf of the Urban Forest Stewardship Initiative (UFSI), a program of Habitat Acquisition Trust (HAT) by Caslys Consulting Ltd.'},
+    {type:'p', content:'RUSH has chosen a diverging colour scheme to highlight areas that meet the 30% canopy cover goal set by Nature Canada. Notice the light yellow areas representing 20% to 30% canopy cover. These could be areas of opportunity for expanding tree equity into more neighbourhoods. Check out the Nature Canada report in the Initiatives section.'},
     {type:'p', content:'Learn more and compare with other years by reading the report here:'},
     {
       type:'link',
@@ -28,7 +29,7 @@ const layer = {
   shape: 'tile',
   symbology: 'classified',
   legendTitle: 'Percentage of Tree Cover',
-  styleMap: styleMap_TreeCoverage2005,
+  styleMap: styleMap,
   questions: [
     { key: 'beat-the-heat', group: StartOnLayersName, active: false },
     { key: 'neighbourhood-to-naturehood', group: StartOffLayersName, active: false },
