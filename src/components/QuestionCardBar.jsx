1af13bf342ed637e3f55fa63f73db5691138dad8
doug-stormtree <doug@stormtree.ca>
2024-07-27 13:35:55 -0700

    Mobile QuestionCardBar button.

--- src/components/QuestionCardBar.jsx
+++ src/components/QuestionCardBar.jsx
@@ -1,10 +1,18 @@
 import React, { useRef } from 'react';
-import { Box, useMultiStyleConfig } from '@chakra-ui/react';
+import {
+  Box,
+  useBreakpointValue,
+  useMultiStyleConfig
+} from '@chakra-ui/react';
 import QuestionCard from './QuestionCard';
 import Questions, { useActiveQuestionStore } from '../data/Questions';
 
 export default function QuestionCardBar() {
-  const styles = useMultiStyleConfig('QuestionCardBar')
+  const screenSize = useBreakpointValue({
+    base: 'mobile',
+    md: 'desktop',
+  })
+  const styles = useMultiStyleConfig('QuestionCardBar', { variant: screenSize })
   const {
     activeQuestion,
     sectionFocus,
@@ -16,30 +24,35 @@ export default function QuestionCardBar() {
   
   return (
     <Box __css={styles.container} ref={scrollRef}>
-      <Box __css={styles.bar}>
-        <QuestionCard
-          key={activeQuestion}
-          question={{key: activeQuestion, ...Questions.get(activeQuestion)}}
-          size={sectionFocus === 1
-            ? 'expanded'
-            : 'wide'
-          }
-          variant='sticky'
-        />
-        {[...Questions.entries()]
-          .filter(q => q[0] !== activeQuestion)
-          .map(q => {
-            return (
-              <QuestionCard
-                key={q[0]}
-                question={{key: q[0], ...q[1]}}
-                size='button'
-                scrollRef={scrollRef}
-              />
-            )
-          })
-        }
+      <Box __css={styles.button}>
+        How can I...?
       </Box>
+      {screenSize === 'desktop' && (
+        <Box __css={styles.bar}>
+          <QuestionCard
+            key={activeQuestion}
+            question={{key: activeQuestion, ...Questions.get(activeQuestion)}}
+            size={sectionFocus === 1
+              ? 'expanded'
+              : 'wide'
+            }
+            variant='sticky'
+          />
+          {[...Questions.entries()]
+            .filter(q => q[0] !== activeQuestion)
+            .map(q => {
+              return (
+                <QuestionCard
+                  key={q[0]}
+                  question={{key: q[0], ...q[1]}}
+                  size='button'
+                  scrollRef={scrollRef}
+                />
+              )
+            })
+          }
+        </Box>
+      )}
     </Box>
   )
 }
--- src/theme/QuestionCardBarTheme.jsx
+++ src/theme/QuestionCardBarTheme.jsx
@@ -22,43 +22,59 @@ const containerGradient = [
 
 const helpers = createMultiStyleConfigHelpers([
   'bar',
+  'button',
   'container',
 ])
 
 const QuestionCardBarTheme = helpers.defineMultiStyleConfig({
   baseStyle: {
+    container: {
+      position:'absolute',
+      
+      zIndex:'9',
+      paddingX: '0.625rem',
+    },
     bar: {
       display: 'flex',
       flexDirection: 'column',
-      paddingStart: '10px',
       paddingY: '10px',
       gap: '10px',
       transform: 'scaleX(-1)', //Flips the child back to normal
-
-      _before: {
-        content: '"How can I...?"',
-        fontFamily: 'var(--chakra-fonts-body)',
-        fontWeight: '500',
-        padding: '0.5rem',
-        borderRadius: 'xl',
-        background: 'white',
-        alignSelf: 'flex-start',
-        position: 'sticky',
-        top: '0.625rem',
-        zIndex: '10',
-      }
     },
-    container: {
-      transform: 'scaleX(-1)', //Reflects the parent horizontally
-      height: 'calc(100% - 40px)',
-      minWidth: '140px',
-      overflow: 'scroll',
-      //background: 'transparent',
-      position:'absolute',
-      top:'2.5rem',
-      zIndex:'9',
-      pointerEvents: 'none',
-      background: containerGradient,
+    button: {
+      fontFamily: 'var(--chakra-fonts-body)',
+      fontWeight: '500',
+      padding: '0.5rem',
+      borderRadius: 'xl',
+      background: 'white',
+      position: 'sticky',
+      top: '0.625rem',
+      zIndex: '10',
+    }
+  },
+  variants: {
+    desktop: {
+      container: {
+        transform: 'scaleX(-1)', //Reflects the parent horizontally
+        background: containerGradient,
+        height: 'calc(100% - 40px)',
+        minWidth: '140px',
+        overflow: 'scroll',
+        pointerEvents: 'none',
+
+        top:'2.5rem',
+      },
+      button: {
+        display: 'block',
+        transform: 'scaleX(-1)'
+
+
+      },
+    },
+    mobile: {
+      container: {
+        bottom: '0.625rem'
+      }
     }
   },
   defaultProps: {
