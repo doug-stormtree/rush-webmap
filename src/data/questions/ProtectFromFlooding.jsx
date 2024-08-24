8752f6e25244320fa9c93d723bf16e80b5ad1471
doug-stormtree <doug@stormtree.ca>
2024-08-24 00:33:02 -0700

    Update ProtectFromFlooding Initiatives

--- src/data/questions/ProtectFromFlooding.jsx
+++ src/data/questions/ProtectFromFlooding.jsx
@@ -1,3 +1,5 @@
+import { InitiativeTags as Tag } from '../TextContent';
+
 const Question = {
   key: 'protect-from-flooding',
   title: 'Protect from Flooding',
@@ -53,57 +55,96 @@ const Question = {
     initiatives: [
       {
         title: "Rain Gardens Story Map",
-        image: require('../png/RainGardenStoryMap.png'),
         link: "https://storymaps.arcgis.com/stories/bade971de06f4502af700ecb844f3fb5",
-        description: "A rain garden is any landscaping that absorbs and filters rainwater runoff from a nearby impervious surface, which is any surface that does not allow water infiltration, such as a roof, driveway, parking lot or street. Learn more in this StoryMap created by UVic Community Mapping students Jared Leary, Aiden Foxcroft, Grace Long, and Olivia Graham."
+        image: require('../png/RainGardenStoryMap.png'),
+        description: "A rain garden is any landscaping that absorbs and filters rainwater runoff from a nearby impervious surface, which is any surface that does not allow water infiltration, such as a roof, driveway, parking lot or street. Learn more in this StoryMap created by UVic Community Mapping students Jared Leary, Aiden Foxcroft, Grace Long, and Olivia Graham.",
+        tags: [
+          Tag.StoryMap,
+        ],
       },
       {
         title: 'Peninsula Streams & Shorelines',
-        image: require('../png/PeninsulaStreams.png'),
         link: 'https://peninsulastreams.ca/',
-        description: 'Peninsula Streams & Shorelines guides communities in organizing to restore and protect the watersheds and shorelines in this region. Their current upstream projects include working with communities and organizations to install rain gardens to manage excess water and mitigate toxic runoff risks to fish. Downstream focus projects include identifying and restoring forage fish beaches that are the foundation of the marine food web. If you want to be the change that tips the scales towards healthy waters, this is the place to skill up. From Headwaters to deep waters, PSS wants to hear your concerns and help your community take action.'
+        image: require('../png/PeninsulaStreams.png'),
+        description: 'Peninsula Streams & Shorelines guides communities in organizing to restore and protect the watersheds and shorelines in this region. Their current upstream projects include working with communities and organizations to install rain gardens to manage excess water and mitigate toxic runoff risks to fish. Downstream focus projects include identifying and restoring forage fish beaches that are the foundation of the marine food web. If you want to be the change that tips the scales towards healthy waters, this is the place to skill up. From Headwaters to deep waters, PSS wants to hear your concerns and help your community take action.',
+        tags: [
+          Tag.Organization,
+        ],
       },
       {
         title: 'Watersheds BC',
-        image: require('../png/WatershedsBC.png'),
         link: 'https://watershedsbc.ca/',
-        description: 'A dedicated resource hub to provide local leaders with knowledge, skills, and support to deliver effective watershed management and governance in their communities. Watersheds BC emerged in response to this need.'
+        image: require('../png/WatershedsBC.png'),
+        description: 'A dedicated resource hub to provide local leaders with knowledge, skills, and support to deliver effective watershed management and governance in their communities. Watersheds BC emerged in response to this need.',
+        tags: [
+          Tag.Portal,
+        ],
       },
       {
         title: 'The Groundwater Project',
-        image: 'https://gw-project.org/wp-content/uploads/2020/08/logo_300x300_a.png',
         link: 'https://gw-project.org/',
-        description: 'Free and online learning resources to make it possible for anyone to understand the issues when it comes to groundwater. This NGO has many collaborators and has its roots in growing Hydrologists without Borders.'
+        image: 'https://gw-project.org/wp-content/uploads/2020/08/logo_300x300_a.png',
+        description: 'Free and online learning resources to make it possible for anyone to understand the issues when it comes to groundwater. This NGO has many collaborators and has its roots in growing Hydrologists without Borders.',
+        tags: [
+          Tag.Portal,
+        ],
       },
       {
         title: 'CRD Watersheds Map',
-        image: require('../png/CRDWatershedMap.png'),
         link: 'https://www.crd.bc.ca/docs/default-source/es-watersheds-pdf/regional-watershed-maps/watershedscrdoverview400dpired.pdf?sfvrsn=5d837aca_2',
-        description: 'A PDF Map of the watershed boundaries within the Capital Regional District. Warning: large file size.'
+        image: require('../png/CRDWatershedMap.png'),
+        description: 'A PDF Map of the watershed boundaries within the Capital Regional District. Warning: large file size.',
+        tags: [
+          Tag.Map,
+          Tag.Government,
+        ],
       },
       {
         title: 'Saanich Peninsula Environmental Coalition',
-        image: require('../png/SPEC.png'),
         link: 'https://saanichpeninsula.ca/',
-        description: 'In respectful collaboration with the W̱SÁNEĆ Nation, the municipalities of Central Saanich, North Saanich and Sidney, the Saanich Peninsula Environmental Coalition is working to integrate the principles and strategies of a bioregional framework into municipal OCPs. Thinking and planning in terms of bioregions - as opposed to political or administrative regions - will better support the overall health of the environment and, by extension, the diversity of human cultures and interests in that bioregion. Through the bioregional lens, the Coalition recommends policy change and invites public response and engagement to develop a stronger, more comprehensive, and citizen-approved approach to ecological sustainability.'
+        image: require('../png/SPEC.png'),
+        description: 'In respectful collaboration with the W̱SÁNEĆ Nation, the municipalities of Central Saanich, North Saanich and Sidney, the Saanich Peninsula Environmental Coalition is working to integrate the principles and strategies of a bioregional framework into municipal OCPs. Thinking and planning in terms of bioregions - as opposed to political or administrative regions - will better support the overall health of the environment and, by extension, the diversity of human cultures and interests in that bioregion. Through the bioregional lens, the Coalition recommends policy change and invites public response and engagement to develop a stronger, more comprehensive, and citizen-approved approach to ecological sustainability.',
+        tags: [
+          Tag.Organization,
+        ],
       },
       {
         title: 'The 1000 Rain Gardens Project',
-        image: require('../png/FriendsOfBowkerCreek.png'),
         link: 'https://bowkercreek.org/1000-rain-gardens/',
-        description: 'The 1000 Rain Gardens Project: "Making Bowker Creek healthy again requires that people send much more rainwater into the earth and much less into the storm drains. Wherever we live, work or go to school in Bowker Creek\'s valley, the 1000 Rain Gardens Project aims to help us manage rainwater better. The project partners Friends of Bowker Creek Society with Peninsula Streams Society (PSS). The PSS initiative, the Rain Gardens in Headwaters Program, will multiply rain gardens throughout Greater Victoria, starting here in Bowker Creek\'s valley. The 1000 Rain Gardens project provides a vital first step and proving ground for the wider program."'
+        image: require('../png/FriendsOfBowkerCreek.png'),
+        description: 'The 1000 Rain Gardens Project: "Making Bowker Creek healthy again requires that people send much more rainwater into the earth and much less into the storm drains. Wherever we live, work or go to school in Bowker Creek\'s valley, the 1000 Rain Gardens Project aims to help us manage rainwater better. The project partners Friends of Bowker Creek Society with Peninsula Streams Society (PSS). The PSS initiative, the Rain Gardens in Headwaters Program, will multiply rain gardens throughout Greater Victoria, starting here in Bowker Creek\'s valley. The 1000 Rain Gardens project provides a vital first step and proving ground for the wider program."',
+        tags: [
+          Tag.Initiative,
+          Tag.Program,
+        ],
       },
       {
         title: 'Colquitz River Restoration Storymap',
         link: 'https://storymaps.arcgis.com/stories/e884cb42946a46d1800e90a692eca278',
+        image: require('../png/ColquitzRiverRestoration.png'),
         description: '"The (ongoing) Colquitz River restoration project, launched by Peninsula Streams and the District of Saanich in 2019, aims to restore ecological integrity and resilience to this urban watershed system in what is now called Saanich, British Columbia, Canada." This story was developed in partnership with Peninsula Streams Society, Engage with Nature-Based Solutions, and UVic Community Mapping students Langley Chan, Olivia Gatrell, Zacc Lavigne, and Nabila Kazmi.',
-        image: require('../png/ColquitzRiverRestoration.png')
+        tags: [
+          Tag.StoryMap,
+        ],
       },
       {
         title: 'Rainwater Rewards Program',
         link: 'https://www.victoria.ca/home-property/utilities/stormwater-management/rainwater-rewards-program',
+        image: require('../png/RainwaterProgram.png'),
         description: 'This incentive program that encourages City of Victoria residents, businesses and institutional properties to use rain was a resource and helping developed watersheds mimic the function of natural systems. Rebates and ongoing credits are available for the cost of installing approved rainwater management methods from cisterns and rain gardens to permeable paving and green roofs. Check out this website to learn more and apply.',
-        image: require('../png/RainwaterProgram.png')
+        tags: [
+          Tag.Funding,
+          Tag.Government,
+        ],
+      },
+      {
+        title: 'FloodWise',
+        link: 'https://floodwise.ca/',
+        image: 'https://floodwise.ca/wp-content/uploads/2022/02/FW-logo-retina.png',
+        description: 'Explore FloodWise to learn about Fraser River flood and coastal flood hazards and risks in the Lower Mainland. This portal provides important maps and resources to help us understand and reduce flood risk. Learn about the work underway to reduce flood risks for the region, as well as ways you can floodproof your own home and business.',
+        tags: [
+          Tag.Portal,
+        ],
       },
     ],
   }
