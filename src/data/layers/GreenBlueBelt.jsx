bcb26569fc5fa2b19abc6a901d9f2f7e588b0175
doug-stormtree <doug@stormtree.ca>
2024-09-26 14:36:08 -0700

    Add GreenBlueBelt description

--- src/data/layers/GreenBlueBelt.jsx
+++ src/data/layers/GreenBlueBelt.jsx
@@ -23,9 +23,10 @@ const styleMap = new Map([
 const layer = {
   title: 'Sea-to-Sea Green Blue Belt',
   description: [
-    {type: 'p', content: ''},
-    {type: 'p', content: ''},
-    {type: 'link', content: 'Native Land Digital', url: 'https://native-land.ca/'},
+    {type: 'p', content: 'The Sea-to-Sea Green Blue Belt is a connected series of parks, greenspace, protected lands, and water bodies between Saanich Inlet and the Sooke Basin. Ecosystem connectivity is essential for forest, wildlife, and marine health. The campaign to establish a protected corridor began in the early 1990s and has been the work of many environmental and community organizations.'},
+    {type: 'link', content: 'Read about the campaign history from this 1999 report by the Wilderness Committee.', url: 'https://www.wildernesscommittee.org/publications/victorias-sea-sea-green-blue-belt'},
+    {type: 'link', content: 'Visit the CRD page for Sea to Sea Regional Park for updates on one of the largest parks in the belt.', url: 'https://www.crd.bc.ca/parks-recreation-culture/parks-trails/find-park-trail/seatosea'},
+    {type: 'link', content: 'Visit the CRD page for the Greater Victoria Water Supply Area to learn about drinking water resevoir lands protection in the region.', url: 'https://www.crd.bc.ca/service/drinking-water/watershed-protection/greater-victoria-water-supply-area'},
   ],
   data: require('../geojson/GreenBlueBelt.geojson'),
   shape: 'polygon',
