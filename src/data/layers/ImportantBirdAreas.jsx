da4fdf95093cca3f21bcdab07b1df6763f775148
doug-stormtree <doug@stormtree.ca>
2025-03-30 14:50:25 -0700

    Add Matson Lands Conservation Area

--- src/data/layers/ImportantBirdAreas.jsx
+++ src/data/layers/ImportantBirdAreas.jsx
@@ -2,7 +2,7 @@ const layer = {
   title: 'Important Bird Areas',
   description: [
     {type: 'p', content: 'In our NatureHood there are three Important Bird & Biodi- versity areas: Sidney Channel and Mandarte Island located next to Shoal Harbour MBS, and Chain Islets - Grand Chain Island located next to Victoria Harbour MBS. There are 84 IBAs in British Columbia. These IBAs are of exceptional international significance for the conservation of birds and biodiversity and are part of a global network first established in 1985. In Canada, IBAs are a joint initiative of Nature Canada and Birds Canada.'},
-    {type: 'image', url: 'https://www.ibacanada.com/images/kml/IBA-Stamp_en.jpg'},
+    {type: 'image', url: 'https://www.ibacanada.com/images/kml/banner_kml.jpg'},
     {type: 'link', content: 'Important Bird and Biodiversity Areas in Canada', url: 'https://www.ibacanada.com'},
     {type: 'p', content: 'Disclaimer: IBA boundaries are generalized approximations reflecting dynamic populations of birds and their habitats. Boundaries are frequently reviewed and may change at any time. These data have been released for public interest and to encourage effective conservation.'},
   ],
