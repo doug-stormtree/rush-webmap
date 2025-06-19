ebc9105662f88a9860388f19d720ca9507055502
Doug Johnson <doug@stormtree.ca>
2025-06-18 20:31:39 -0700

    Add Mapbox Search Box

--- src/components/PlacesAutocomplete.jsx
+++ src/components/PlacesAutocomplete.jsx
@@ -1,95 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Input, useBreakpointValue } from '@chakra-ui/react';
-import { latLng, latLngBounds, marker } from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import {  useBreakpointValue } from '@chakra-ui/react';
 import { useMap } from 'react-leaflet';
 import Control from 'react-leaflet-custom-control';
-import { Loader } from '@googlemaps/js-api-loader';
 import { mapPopupContent } from '../data/LeafletStyleHelpers';
+import { SearchBox } from '@mapbox/search-js-react';
+import { latLng, marker } from 'leaflet';
 
-const leafletLatLngFromGoogle = (googleLatLng) => {
-  return latLng(googleLatLng.lat(), googleLatLng.lng())
-}
-
-const leafletBoundsFromGoogle = (googleLatLngBounds) => {
-  return latLngBounds(
-    leafletLatLngFromGoogle(googleLatLngBounds.getSouthWest()),
-    leafletLatLngFromGoogle(googleLatLngBounds.getNorthEast())
-  )
-}
-
-export const PlacesAutocomplete = ({ isMobile }) => {
-  const map = useMap()
-  const inputRef = useRef(null)
-  const [ autocomplete, setAutocomplete ] = useState(null);
-  
-  useEffect(() => {
-    let ignore = false
-    if (inputRef === null) return
-    if (autocomplete === null) {
-      const loader = new Loader({
-        apiKey: process.env.REACT_APP_GOOGLE_PLACES_KEY,
-        version: 'weekly',
-      })
-
-      loader
-        .importLibrary('places')
-        .then(({ Autocomplete }) => {
-          const opts = {
-            fields: ['place_id', 'name', 'geometry' ],
-            componentRestrictions: { country: 'ca' }
-          }
-          const autocomplete = new Autocomplete(inputRef.current, opts)
-          if (ignore) return
-          setAutocomplete(autocomplete)
-        })
-    }
-
-    return () => {
-      ignore = true
-    }
-  }, [ autocomplete, inputRef ])
+export const PlacesAutocomplete = () => {
+  const map = useMap();
+  const [placeMarker, setPlaceMarker] = useState(null);
+  const [mapCenter, setMapCenter] = useState(null);
 
   useEffect(() => {
     if (map === undefined) return
-    if (autocomplete === null) return
-
-    // Add google listener to fly map to marker when place selected
-    let placeMarker = null
-    const onPlaceChanged = () => {
-      const place = autocomplete.getPlace()
-      if (place?.geometry.location) {
-        // convert google location to leaflet
-        const placeLatLng = leafletLatLngFromGoogle(place.geometry.location)
-        
-        // add a marker to the map
-        placeMarker = marker(placeLatLng)
-        placeMarker.bindPopup(mapPopupContent(
-          place.name,
-          ), {offset: [0,2]});
-        map.addLayer(placeMarker)
-
-        // fly the map to the ideal bounds, or to the location with default zoom
-        if (place?.geometry.viewport) {
-          map.flyToBounds(
-            leafletBoundsFromGoogle(place.geometry.viewport),
-            { maxZoom: 16 }
-          )
-        } else {
-          map.flyTo(placeLatLng, 14)
-        }
-      }
-    }
-    autocomplete.addListener('place_changed', onPlaceChanged)
 
     // Add leaflet onmove listener to update autocomplete viewport restrictions
     const onMoveOrZoom = () => {
-      const bounds = map.getBounds()
-      autocomplete.setBounds({
-        east: bounds.getEast(),
-        north: bounds.getNorth(),
-        south: bounds.getSouth(),
-        west: bounds.getWest(),
-      })
+      const center = map.getCenter()
+      setMapCenter({lat: center.lat, lng: center.lng})
     }
     map.on('moveend zoomend', onMoveOrZoom)
 
@@ -100,7 +28,7 @@ export const PlacesAutocomplete = ({ isMobile }) => {
       if (placeMarker) map.removeLayer(placeMarker)
       map.off('moveend zoomend', onMoveOrZoom)
     }
-  }, [ autocomplete, map ])
+  }, [ map, placeMarker, setMapCenter ])
 
   const placeholderText = useBreakpointValue({
     base: 'Search...',
@@ -116,19 +44,44 @@ export const PlacesAutocomplete = ({ isMobile }) => {
     base: 'topright',
     lg: 'topleft',
   }, {ssr:false, fallback:true});
+
+  const onRetrieve = (res) => {
+    const place = res.features[0];
+    if (place?.geometry.coordinates) {
+      // convert location to leaflet latlng
+      const placeLatLng = latLng(
+        place.geometry.coordinates[1],
+        place.geometry.coordinates[0]
+      )
+      
+      // add a marker to the map
+      const placeMarker = marker(placeLatLng)
+      placeMarker.bindPopup(mapPopupContent(
+        place.properties.name,
+        ), {offset: [0,2]});
+      map.addLayer(placeMarker)
+      map.flyTo(placeLatLng, 14)
+      setPlaceMarker(placeMarker)
+    }
+  }
   
   return (
     <Control prepend position={inputPosition}>
-      <Input
-        ref={inputRef}
-        width={inputWidth}
-        marginStart='-0.625rem'
-        borderRadius='xl'
-        bgColor='gray.100'
-        fontFamily='var(--chakra-fonts-body)'
+      <SearchBox
+        accessToken='pk.eyJ1IjoicnVzaGFkbWluIiwiYSI6ImNtYzJudWd6czBhNTkybHEzNHdpNGE1MTUifQ.T-8P_6hh3kai9tTzjtvcTQ'
         placeholder={placeholderText}
-        hidden={isMobile}
-        _placeholder={{color: 'orange.500', opacity: '0.8'}}
+        onRetrieve={onRetrieve}
+        options={{
+          language: 'en',
+          country: 'CA',
+          proximity: mapCenter
+        }}
+        popoverOptions={{
+          offset: 5
+        }}
+        theme={{
+          cssText: `.Results {left: 0} .MapboxSearch {width: ${inputWidth}}`
+        }}
       />
     </Control>
   )
