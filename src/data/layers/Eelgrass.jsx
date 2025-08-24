4febc2660e8bd20468a2117af64fa55c4cf879f8
Doug Johnson <doug@stormtree.ca>
2025-08-23 22:23:35 -0700

    Update Eelgrass description to include anchoring info

--- src/data/layers/Eelgrass.jsx
+++ src/data/layers/Eelgrass.jsx
@@ -2,11 +2,12 @@ import { circle } from 'leaflet';
 
 const eelgrassColor = '#38a801';
 
-
 const layer = {
   title: "Eelgrass",
   description: [
-    {type: 'p', content: 'This layer depicts locations of known eelgrass areas within the CRD. Each point symbolizes eelgrass within 50 meters. Data for this layer was sourced from Shorezone’s 2013 eelgrass data, SeaChange’s RESS maps, and the CRD’s published eelgrass survey.'},
+    {type: 'p', content: 'This layer depicts locations of known eelgrass areas within the CRD. Each point symbolizes eelgrass within 50 meters. When boating, it’s recommended to avoid anchoring in eelgrass beds to protect this important habitat. Learn more about protecting eelgrass from:'},
+    {type: 'link', url: 'https://resilientcoasts.ca/resource/protecting-eelgrass-when-boating/', content: 'Resilient Coasts for Salmon'},
+    {type: 'p', content: 'Data for this layer was sourced from Shorezone’s 2013 eelgrass data, SeaChange’s RESS maps, and the CRD’s published eelgrass survey.'},
     {type: 'link', url: 'https://www.shorezone.org/downloadzone/', content: 'Shorezone'},
     {type: 'link', url: 'https://seachangesociety.com/current-projects/resilient-estuaries-of-the-salish-sea-project-ress/', content: 'SeaChange Resilient Estuaries of the Salish Sea Project'},
     {type: 'link', url: 'https://mapservices.crd.bc.ca/arcgis/rest/services/Harbours/MapServer/25', content: 'CRD Eelgrass Survey Data'},
